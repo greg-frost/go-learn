d9d495982f016e83fd13d6463047374d80193e86
Григорий Морозов <greg-frost@yandex.ru>
2023-12-01 21:06:41 +0300

    Fix heap

--- heap/main.go
+++ heap/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
+
+/* Моя */
 
 type Heap []int
 
@@ -42,42 +45,116 @@ func (h *Heap) PopMax() (int, bool) {
 	return v, true
 }
 
+/* Стандартная */
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
 func main() {
 	fmt.Println(" \n[ КУЧА ]\n ")
 
-	heap := Heap{}
+	/* Моя */
+
+	fmt.Println("Моя")
+	fmt.Println("---")
+	fmt.Println()
+
+	h1 := Heap{}
 
 	// Добавление
 
 	fmt.Println("Push: 3")
-	heap.Push(3)
+	h1.Push(3)
 
 	fmt.Println("Push: 5")
-	heap.Push(5)
+	h1.Push(5)
 
 	fmt.Println("Push: 1")
-	heap.Push(1)
+	h1.Push(1)
 
 	// Состояние
 	fmt.Println()
-	fmt.Println("Heap:", heap)
+	fmt.Println("Heap:", h1)
 	fmt.Println()
 
 	// Извлечение
 
-	v, ok := heap.PopMin()
+	v, ok := h1.PopMin()
 	fmt.Println("Min:", v, ok)
 
-	v, ok = heap.PopMax()
+	v, ok = h1.PopMax()
 	fmt.Println("Max:", v, ok)
 
-	v, ok = heap.PopMin()
+	v, ok = h1.PopMin()
 	fmt.Println("Min:", v, ok)
 
-	v, ok = heap.PopMax()
+	v, ok = h1.PopMax()
 	fmt.Println("Max:", v, ok)
 
 	// Состояние
 	fmt.Println()
-	fmt.Println("Heap:", heap)
+	fmt.Println("Heap:", h1)
+	fmt.Println()
+
+	/* Стандартная */
+
+	fmt.Println("Стандартная")
+	fmt.Println("-----------")
+	fmt.Println()
+
+	h2 := &IntHeap{}
+	heap.Init(h2)
+
+	// Добавление
+
+	fmt.Println("Push: 10")
+	heap.Push(h2, 10)
+
+	fmt.Println("Push: 5")
+	heap.Push(h2, 5)
+
+	fmt.Println("Push: 15")
+	heap.Push(h2, 15)
+
+	// Состояние
+	fmt.Println()
+	fmt.Println("Heap:", *h2)
+	fmt.Println()
+
+	// Извлечение
+
+	v = heap.Pop(h2).(int)
+	fmt.Println("Min:", v)
+
+	v = heap.Pop(h2).(int)
+	fmt.Println("Min:", v)
+
+	v = heap.Pop(h2).(int)
+	fmt.Println("Min:", v)
+
+	// Состояние
+	fmt.Println()
+	fmt.Println("Heap:", *h2)
+
 }
