fe9ae07c2a9bb0ff7d3b7376c74c5946571c749a
Григорий Морозов <greg-frost@yandex.ru>
2024-05-25 13:07:33 +0300

    Fix gin

--- gin/main.go
+++ gin/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Структура "Альбом"
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -14,38 +15,19 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+// Данные альбомов
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func main() {
-	fmt.Println(" \n[ GIN ]\n ")
-
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
-
-	router.Run("localhost:8080")
-}
-
+// Получение всех альбомов
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
+// Получение альбома по ID
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
@@ -57,4 +39,31 @@ func getAlbumByID(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "альбом не найден"})
+}
+
+// Создание альбома
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func main() {
+	fmt.Println(" \n[ GIN ]\n ")
+
+	// Создание роутера
+	router := gin.Default()
+
+	// Обработчики
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbum)
+
+	// Запуск сервера
+	router.Run("localhost:8080")
 }
