dd2158f391821b1af106025f6b10003d34f19ca4
Григорий Морозов <greg-frost@yandex.ru>
2024-05-30 18:14:01 +0300

    Learn paths

---
+++ paths/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	fmt.Println(" \n[ ПУТИ ]\n ")
+
+	/* Пути */
+
+	fmt.Println("Объединение:")
+	path := filepath.Join("dir", "sub", "file")
+	fmt.Println(path)
+	fmt.Println(filepath.Join("sub//", "file"))
+	fmt.Println(filepath.Join("dir/../dir", "file"))
+	fmt.Println()
+
+	fmt.Println("Фрагменты:")
+	fmt.Println("Dir:", filepath.Dir(path))
+	fmt.Println("Base:", filepath.Base(path))
+	fmt.Println()
+
+	fmt.Println("Относительный:", filepath.IsAbs("dir/file"))
+	fmt.Println("Абсолютный (Linux):", filepath.IsAbs("/dir/file"))
+	fmt.Println("Абсолютный (Windows):", filepath.IsAbs("C:\\Go\\Lang"))
+	fmt.Println()
+
+	/* Файлы */
+
+	filename := "config.json"
+	ext := filepath.Ext(filename)
+	fmt.Println("Файл:", filename)
+	fmt.Println("Имя:", strings.TrimSuffix(filename, ext))
+	fmt.Println("Расширение:", ext)
+	fmt.Println()
+
+	/* Отношения */
+
+	rel, err := filepath.Rel("dir/sub1", "dir/sub1/sub2/file")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dir/sub1 > sub2/file:", rel)
+
+	rel, err = filepath.Rel("dir/sub1", "dir/sub2/sub3/file")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dir/sub1 <> sub2/sub3/file:", rel)
+}
