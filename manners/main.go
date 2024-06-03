9d94d91b9a3d0a077d3c0d336ee1898c879b8345
Григорий Морозов <greg-frost@yandex.ru>
2024-06-03 22:34:28 +0300

    Learn manners

---
+++ manners/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/braintree/manners"
+)
+
+// Структура "обработчик"
+type handler struct{}
+
+// Получение нового обработчика
+func newHandler() *handler {
+	return &handler{}
+}
+
+// Обработка HTTP-запроса
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "Мир"
+	}
+	fmt.Fprintf(w, "Привет, %s!", name)
+}
+
+// Ожидание завершения
+func listenForShutdown(ch <-chan os.Signal) {
+	<-ch
+	fmt.Print("Завершение работы...")
+	manners.Close()
+}
+
+func main() {
+	fmt.Println(" \n[ MANNERS ]\n ")
+
+	// Обработчик
+	handler := newHandler()
+
+	// Планирование мягкого завершения
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt, os.Kill)
+	go listenForShutdown(ch)
+
+	// Запуск сервера
+	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("(на http://localhost:8080)")
+	manners.ListenAndServe("localhost:8080", handler)
+}
