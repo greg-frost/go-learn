422605594f95484ff1bd56523ac5c70271466860
Григорий Морозов <greg-frost@yandex.ru>
2024-06-03 19:13:20 +0300

    Learn toml

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
---
+++ toml/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/BurntSushi/toml"
+)
+
+func main() {
+	fmt.Println(" \n[ TOML-КОНФИГУРАЦИЯ ]\n ")
+
+	// Структура для хранения опций
+	config := struct {
+		Title string
+		User  struct {
+			Id       int
+			Rate     float64
+			Name     string
+			Birthday time.Time
+		}
+		Db struct {
+			Server  string
+			Ports   []int
+			Enabled bool
+		}
+		Servers struct {
+			Alpha struct {
+				Ip string
+				Dc string
+			}
+			Omega struct {
+				Ip string
+				Dc string
+			}
+		}
+		Clients struct {
+			Data  [][]string
+			Hosts []string
+		}
+	}{}
+
+	// Чтение конфигурации
+	path := os.Getenv("GOPATH") + "/src/golearn/toml/"
+	if _, err := toml.DecodeFile(path+"example.toml", &config); err != nil {
+		log.Fatal(err)
+	}
+
+	/* Вывод */
+
+	fmt.Printf("Title: %v (%T)\n", config.Title, config.Title)
+	fmt.Println()
+
+	fmt.Println("User:")
+	fmt.Printf("   ID: %v (%T)\n", config.User.Id, config.User.Id)
+	fmt.Printf("   Rate: %v (%T)\n", config.User.Rate, config.User.Rate)
+	fmt.Printf("   Name: %v (%T)\n", config.User.Name, config.User.Name)
+	fmt.Printf("   Birthday: %v (%T)\n", config.User.Birthday, config.User.Birthday)
+	fmt.Println()
+
+	fmt.Println("Db:")
+	fmt.Printf("   Server: %v (%T)\n", config.Db.Server, config.Db.Server)
+	fmt.Printf("   Ports: %v (%T)\n", config.Db.Ports, config.Db.Ports)
+	fmt.Printf("   Enabled: %v (%T)\n", config.Db.Enabled, config.Db.Enabled)
+	fmt.Println()
+
+	fmt.Println("Servers:")
+	fmt.Printf("   Alpha:\n")
+	fmt.Printf("      IP: %v (%T)\n", config.Servers.Alpha.Ip, config.Servers.Alpha.Ip)
+	fmt.Printf("      DC: %v (%T)\n", config.Servers.Alpha.Dc, config.Servers.Alpha.Dc)
+	fmt.Printf("   Omega:\n")
+	fmt.Printf("      IP: %v (%T)\n", config.Servers.Omega.Ip, config.Servers.Omega.Ip)
+	fmt.Printf("      DC: %v (%T)\n", config.Servers.Omega.Dc, config.Servers.Omega.Dc)
+	fmt.Println()
+
+	fmt.Println("Clients:")
+	fmt.Printf("   Data: %v (%T)\n", config.Clients.Data, config.Clients.Data)
+	fmt.Printf("   Hosts: %v (%T)\n", config.Clients.Hosts, config.Clients.Hosts)
+}
