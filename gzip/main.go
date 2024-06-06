028453d5bbd94f6769a98fe5b68c5e905d9e1b36
Григорий Морозов <greg-frost@yandex.ru>
2024-06-06 15:53:50 +0300

    Learn gzip

---
+++ gzip/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+func main() {
+	fmt.Println(" \n[ GZIP-КОМПРЕССИЯ ]\n ")
+
+	// Смена директории
+	path := os.Getenv("GOPATH") + "/src/golearn/gzip/"
+	os.Chdir(path)
+
+	if len(os.Args) == 1 {
+		fmt.Println("Передайте список файлов в виде параметров!")
+		return
+	}
+
+	fmt.Println("Идет gzip-компрессия...")
+
+	// Параллельное сжатие
+	var wg sync.WaitGroup
+	var count int
+	for _, file := range os.Args[1:] {
+		wg.Add(1)
+		go func(filename string) {
+			defer wg.Done()
+			compress(filename)
+		}(file)
+		count++
+	}
+	wg.Wait()
+
+	fmt.Printf("Обработано файлов: %d\n", count)
+}
+
+// Сжатие файла
+func compress(filename string) error {
+	in, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(filename + ".gz")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	gzout := gzip.NewWriter(out)
+	_, err = io.Copy(gzout, in)
+	defer gzout.Close()
+
+	return err
+}
