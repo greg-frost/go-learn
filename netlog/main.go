c77c76e05eca90714f88d679a40da6c77fa5dce1
Григорий Морозов <greg-frost@yandex.ru>
2024-06-14 13:00:28 +0300

    Fix netlog

--- netlog/main.go
+++ netlog/main.go
@@ -7,24 +7,25 @@ import (
 	"time"
 )
 
-// Сервер
-func server(protocol, address string) {
-	ln, err := net.Listen(protocol, address)
+// TCP-сервер
+func tcpServer(protocol, address string) {
+	conn, err := net.Listen(protocol, address)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer conn.Close()
 	for {
-		c, err := ln.Accept()
+		c, err := conn.Accept()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		go connection(c)
+		go tcpConnection(c)
 	}
 }
 
-// Соединение
-func connection(c net.Conn) {
+// TCP-соединение
+func tcpConnection(c net.Conn) {
 	defer c.Close()
 	for {
 		msg := make([]byte, 1024)
@@ -39,25 +40,64 @@ func connection(c net.Conn) {
 	}
 }
 
+// UDP-сервер
+func udpServer(protocol, address string) {
+	conn, err := net.ListenPacket(protocol, address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	for {
+		go udpConnection(conn)
+	}
+}
+
+// UDP-соединение
+func udpConnection(c net.PacketConn) {
+	msg := make([]byte, 1024)
+	n, _, err := c.ReadFrom(msg)
+	if err != nil {
+		return
+	}
+	fmt.Print(string(msg[:n]))
+}
+
 func main() {
 	fmt.Println(" \n[ СЕТЕВОЕ ЛОГИРОВАНИЕ ]\n ")
 
-	// Запуск сервера
-	go server("tcp", "localhost:8080")
+	// Запуск серверов
+	go tcpServer("tcp", "localhost:8080")
+	go udpServer("udp", "localhost:9090")
 	time.Sleep(100 * time.Millisecond)
 
 	timeout := 30 * time.Second
 	flags := log.LstdFlags | log.Lshortfile
 
 	// Логирование по TCP
-	conn, err := net.DialTimeout("tcp", "localhost:8080", timeout)
+	tcp, err := net.DialTimeout("tcp", "localhost:8080", timeout)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
+	defer tcp.Close()
 
 	fmt.Println("Протокол TCP:")
-	logger := log.New(conn, "[tcp] ", flags)
+	logger := log.New(tcp, "[tcp] ", flags)
+	logger.Print("Hello")
+	logger.Printf("Cruel\n")
+	logger.Println("World")
+
+	time.Sleep(250 * time.Millisecond)
+	fmt.Println()
+
+	// Логирование по UDP
+	udp, err := net.DialTimeout("udp", "localhost:9090", timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer udp.Close()
+
+	fmt.Println("Протокол UDP:")
+	logger = log.New(udp, "[udp] ", flags)
 	logger.Print("Hello")
 	logger.Printf("Cruel\n")
 	logger.Println("World")
