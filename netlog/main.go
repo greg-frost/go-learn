6518515da0bec8d6fe6a5045b262b7e024d1549e
Григорий Морозов <greg-frost@yandex.ru>
2024-06-17 10:05:42 +0300

    Fix netlog

--- netlog/main.go
+++ netlog/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // TCP-сервер
-func tcpServer(protocol, address string) {
+func tcpServer(protocol, address string, print bool) {
 	conn, err := net.Listen(protocol, address)
 	if err != nil {
 		log.Fatal(err)
@@ -20,58 +20,61 @@ func tcpServer(protocol, address string) {
 			fmt.Println(err)
 			continue
 		}
-		go tcpConnection(c)
+		go tcpConnection(c, print)
 	}
 }
 
 // TCP-соединение
-func tcpConnection(c net.Conn) {
+func tcpConnection(c net.Conn, print bool) {
 	defer c.Close()
 	for {
 		msg := make([]byte, 1024)
 		n, err := c.Read(msg)
 		if err != nil {
-			log.Fatal(err)
-		}
-		if n == 0 {
 			return
 		}
-		fmt.Print(string(msg[:n]))
+		if print {
+			fmt.Print(string(msg[:n]))
+		}
 	}
 }
 
 // UDP-сервер
-func udpServer(protocol, address string) {
+func udpServer(protocol, address string, print bool) {
 	conn, err := net.ListenPacket(protocol, address)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 	for {
-		go udpConnection(conn)
+		udpConnection(conn, print)
 	}
 }
 
 // UDP-соединение
-func udpConnection(c net.PacketConn) {
+func udpConnection(c net.PacketConn, print bool) {
 	msg := make([]byte, 1024)
 	n, _, err := c.ReadFrom(msg)
 	if err != nil {
 		return
 	}
-	fmt.Print(string(msg[:n]))
+	if print {
+		fmt.Print(string(msg[:n]))
+	}
 }
 
 func main() {
 	fmt.Println(" \n[ СЕТЕВОЕ ЛОГИРОВАНИЕ ]\n ")
 
-	// Запуск серверов
-	go tcpServer("tcp", "localhost:8080")
-	go udpServer("udp", "localhost:9090")
-	time.Sleep(100 * time.Millisecond)
-
 	timeout := 30 * time.Second
 	flags := log.LstdFlags | log.Lshortfile
+
+	/* Использование */
+
+	// Запуск серверов
+	go tcpServer("tcp", "localhost:8080", true)
+	go udpServer("udp", "localhost:9090", true)
+	time.Sleep(250 * time.Millisecond)
 
 	// Логирование по TCP
 	tcp, err := net.DialTimeout("tcp", "localhost:8080", timeout)
@@ -86,7 +89,7 @@ func main() {
 	logger.Printf("Cruel\n")
 	logger.Println("World")
 
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 	fmt.Println()
 
 	// Логирование по UDP
@@ -102,5 +105,54 @@ func main() {
 	logger.Printf("Cruel\n")
 	logger.Println("World")
 
+	time.Sleep(500 * time.Millisecond)
+
+	/* Сравнение скорости */
+
+	times := 100000
+
+	// Запуск серверов
+	go tcpServer("tcp", "localhost:8085", false)
+	go udpServer("udp", "localhost:9095", false)
 	time.Sleep(250 * time.Millisecond)
+
+	fmt.Println()
+	fmt.Println("Сравнение скорости")
+	fmt.Println("------------------")
+	fmt.Println()
+
+	// Логирование по TCP
+	tcp, err = net.DialTimeout("tcp", "localhost:8085", timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tcp.Close()
+
+	fmt.Println("Протокол TCP:")
+	logger = log.New(tcp, "[tcp] ", flags)
+	start := time.Now()
+	for i := 1; i <= times; i++ {
+		logger.Print(i)
+		if i == 1000 || i == 10000 || i == 100000 || i == 1000000 {
+			fmt.Printf("%7d: %v\n", i, time.Now().Sub(start))
+		}
+	}
+	fmt.Println()
+
+	// Логирование по UDP
+	udp, err = net.DialTimeout("udp", "localhost:9095", timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer udp.Close()
+
+	fmt.Println("Протокол UDP:")
+	logger = log.New(udp, "[udp] ", flags)
+	start = time.Now()
+	for i := 1; i <= times; i++ {
+		logger.Print(i)
+		if i == 1000 || i == 10000 || i == 100000 || i == 1000000 {
+			fmt.Printf("%7d: %v\n", i, time.Now().Sub(start))
+		}
+	}
 }
