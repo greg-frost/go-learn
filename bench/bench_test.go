a6362ef7d536201201ed92fc5544d0645905a847
Григорий Морозов <greg-frost@yandex.ru>
2024-06-21 19:22:13 +0300

    Fix bench

--- bench/bench_test.go
+++ bench/bench_test.go
@@ -41,6 +41,7 @@ func BenchmarkFileLen(b *testing.B) {
 	}
 }
 
+// Бенчмарк шаблонов
 func BenchmarkTemplates(b *testing.B) {
 	b.Logf("b.N = %d\n", b.N)
 	tpl := "Hello {{.Name}}"
@@ -50,6 +51,21 @@ func BenchmarkTemplates(b *testing.B) {
 	var buf bytes.Buffer
 	for i := 0; i < b.N; i++ {
 		t, _ := template.New("test").Parse(tpl)
+		t.Execute(&buf, data)
+		buf.Reset()
+	}
+}
+
+// Бенчмарк скомпилированных шаблонов
+func BenchmarkCompiledTemplates(b *testing.B) {
+	b.Logf("b.N = %d\n", b.N)
+	tpl := "Hello {{.Name}}"
+	t, _ := template.New("test").Parse(tpl)
+	data := &map[string]string{
+		"Name": "World",
+	}
+	var buf bytes.Buffer
+	for i := 0; i < b.N; i++ {
 		t.Execute(&buf, data)
 		buf.Reset()
 	}
