13fcb73603f213d151f5da88fb9b46e770b23715
Григорий Морозов <greg-frost@yandex.ru>
2024-06-25 18:11:40 +0300

    Fix templates2

--- templates2/main.go
+++ templates2/main.go
@@ -14,15 +14,16 @@ type Page struct {
 	Content string
 }
 
+// Путь и шаблон
+var path = os.Getenv("GOPATH") + "/src/golearn/templates2/"
+var t = template.Must(template.ParseFiles(path + "simple.html"))
+
 // Обработчик шаблона страницы
 func handlePage(w http.ResponseWriter, r *http.Request) {
 	page := &Page{
 		Title:   "HTML-шаблон",
 		Content: "Данная страница была сгенерирована в Go!",
 	}
-
-	path := os.Getenv("GOPATH") + "/src/golearn/templates2/"
-	t := template.Must(template.ParseFiles(path + "simple.html"))
 	t.Execute(w, page)
 }
 
