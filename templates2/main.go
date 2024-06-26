5ec7a09e445bdeb185adc493f766870c6c493e13
Григорий Морозов <greg-frost@yandex.ru>
2024-06-26 20:47:48 +0300

    Fix templates2

--- templates2/main.go
+++ templates2/main.go
@@ -10,27 +10,28 @@ import (
 	"time"
 )
 
-// Структура "страница"
-type Page struct {
-	Title   string
-	Content string
-	Date    time.Time
-}
-
-// Путь и шаблон
-var path = os.Getenv("GOPATH") + "/src/golearn/templates2/"
-var t *template.Template
+// Путь и шаблоны
+var path = os.Getenv("GOPATH") + "/src/golearn/templates2/tmpl/"
+var t = map[string]*template.Template{}
 
 // Инициализация
 func init() {
-	t = parseTemplate("simple.html")
+	t["simple"] = parseTemplate("simple.html")
+	t["page"] = parseTemplate("base.html", "page.html")
+	t["user"] = parseTemplate("base.html", "user.html")
 }
 
 // Парсинг шаблона
-func parseTemplate(filename string) *template.Template {
-	t := template.New(filename)
+func parseTemplate(filenames ...string) *template.Template {
+	if len(filenames) == 0 {
+		return nil
+	}
+	t := template.New(filenames[0])
 	t.Funcs(funcMap)
-	template.Must(t.ParseFiles(path + filename))
+	for i := 0; i < len(filenames); i++ {
+		filenames[i] = path + filenames[i]
+	}
+	template.Must(t.ParseFiles(filenames...))
 	return t
 }
 
@@ -44,15 +45,28 @@ func dateFormat(layout string, d time.Time) string {
 	return d.Format(layout)
 }
 
-// Обработчик шаблона страницы
-func handlePage(w http.ResponseWriter, r *http.Request) {
+// Структура "страница"
+type Page struct {
+	Title   string
+	Content string
+	Date    time.Time
+}
+
+// Структура "пользователь"
+type User struct {
+	Username string
+	Name     string
+}
+
+// Обработчик простой страницы
+func handleSimple(w http.ResponseWriter, r *http.Request) {
 	page := &Page{
 		Title:   "HTML-шаблон",
 		Content: "Данная страница была сгенерирована в Go!",
 		Date:    time.Now(),
 	}
 	var b bytes.Buffer
-	err := t.Execute(&b, page)
+	err := t["simple"].Execute(&b, page)
 	if err != nil {
 		fmt.Fprint(w, "Ошибка выполнения шаблона")
 		return
@@ -60,11 +74,31 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	b.WriteTo(w)
 }
 
+// Обработчик составной страницы
+func handlePage(w http.ResponseWriter, r *http.Request) {
+	page := &Page{
+		Title:   "Составная страница",
+		Content: "Данная страница была собрана из разных шаблонов, прямо как Франкенштейн...",
+	}
+	t["page"].ExecuteTemplate(w, "base", page)
+}
+
+// Обработчик составной страницы пользователя
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	user := &User{
+		Username: "greg_frost",
+		Name:     "Морозов Григорий",
+	}
+	t["user"].ExecuteTemplate(w, "base", user)
+}
+
 func main() {
 	fmt.Println(" \n[ HTML-ШАБЛОНЫ ]\n ")
 
-	// Обработчик
-	http.HandleFunc("/", handlePage)
+	// Обработчики
+	http.HandleFunc("/", handleSimple)
+	http.HandleFunc("/page", handlePage)
+	http.HandleFunc("/user", handleUser)
 
 	// Запуск сервера
 	fmt.Println("Ожидаю обновлений...")
