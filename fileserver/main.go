5cdd77fb5c46d504f442a1e723639473f97c68ee
Григорий Морозов <greg-frost@yandex.ru>
2024-07-01 17:54:04 +0300

    Fix fileserver

--- fileserver/main.go
+++ fileserver/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	//fs "github.com/Masterminds/go-fileserver"
 )
 
 // Путь
@@ -28,8 +29,16 @@ func main() {
 		),
 	)
 
+	// Пользовательский файл-сервер
+	// с настраиваемой страницей ошибок
+	// fs.NotFoundHandler = func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// 	fmt.Fprintln(w, "Ошибка 404 - Страница не найдена!")
+	// }
+
 	// Запуск сервера
 	fmt.Println("Ожидаю обновлений...")
 	fmt.Println("(на http://localhost:8080)")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	//log.Fatal(http.ListenAndServe("localhost:8080", fs.FileServer(http.Dir(path+"test"))))
 }
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/Masterminds/go-fileserver v0.0.0-20150404113552-04d61ae6d130 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
