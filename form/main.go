be0c2d400e2b127af9db6a48aefbaaa05309c15a
Григорий Морозов <greg-frost@yandex.ru>
2024-07-02 19:08:24 +0300

    Learn form

---
+++ form/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Путь и шаблон
+var path = os.Getenv("GOPATH") + "/src/golearn/form/"
+var t = template.Must(template.ParseFiles(path + "form.html"))
+
+// Структура "страница"
+type Page struct {
+	Title string
+	Name  string
+	Langs []string
+}
+
+// Обработчик формы
+func handleForm(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := r.FormValue("name")
+	if name == "" {
+		name = r.PostFormValue("name")
+	}
+
+	var langs []string
+	for _, v := range r.Form["langs"] {
+		langs = append(langs, v)
+	}
+
+	page := &Page{
+		Title: "HTML-форма",
+		Name:  name,
+		Langs: langs,
+	}
+	t.Execute(w, page)
+}
+
+func main() {
+	fmt.Println(" \n[ HTML-ФОРМА ]\n ")
+
+	// Обработчик
+	http.HandleFunc("/", handleForm)
+
+	// Запуск сервера
+	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("(на http://localhost:8080)")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
