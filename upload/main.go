f621e51f7634f1339d4d637a1ee71b8074ee032a
Григорий Морозов <greg-frost@yandex.ru>
2024-07-03 19:51:57 +0300

    Learn upload

---
+++ upload/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Путь и шаблон
+var path = os.Getenv("GOPATH") + "/src/golearn/upload/"
+var t = template.Must(template.ParseFiles(path + "form.html"))
+
+// Обработчик загрузки
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t.Execute(w, nil)
+		return
+	}
+
+	in, h, err := r.FormFile("file")
+	if err != nil {
+		fmt.Fprint(w, "Файл не был загружен...")
+		return
+	}
+	defer in.Close()
+
+	filename := path + h.Filename
+	out, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	io.Copy(out, in)
+
+	contentType := h.Header["Content-Type"][0]
+	w.Header().Set("Content-Type", contentType)
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(b)
+
+	go deleteFile(filename, 3*time.Second)
+}
+
+// Отложенное удаление файла
+func deleteFile(filename string, delay time.Duration) {
+	time.Sleep(delay)
+	os.Remove(filename)
+}
+
+func main() {
+	fmt.Println(" \n[ ЗАГРУЗКА ФАЙЛОВ ]\n ")
+
+	// Обработчик
+	http.HandleFunc("/", handleUpload)
+
+	// Запуск сервера
+	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("(на http://localhost:8080)")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
