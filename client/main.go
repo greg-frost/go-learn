c1cd496e8d68d13d9b5ec98c039a35ffaa278fc1
Григорий Морозов <greg-frost@yandex.ru>
2024-07-08 17:17:01 +0300

    Fix client

--- client/main.go
+++ client/main.go
@@ -6,25 +6,30 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	fmt.Println(" \n[ HTTP-КЛИЕНТ ]\n ")
 
-	// Запрос
-	resp, err := http.Get("https://go.dev")
+	// GET-запрос
+
+	addr := "https://go.dev"
+	fmt.Println("GET:", addr)
+
+	get, err := http.Get(addr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer get.Body.Close()
 
 	// Статус ответа
-	fmt.Println("Статус ответа:", resp.Status)
+	fmt.Println("Статус ответа:", get.Status)
 	fmt.Println()
 
 	// Чтение построчно
 	fmt.Println("Первые строки:")
-	scanner := bufio.NewScanner(resp.Body)
+	scanner := bufio.NewScanner(get.Body)
 	for i := 0; scanner.Scan() && i < 3; i++ {
 		fmt.Println(scanner.Text())
 	}
@@ -35,9 +40,25 @@ func main() {
 	fmt.Println()
 
 	// Чтение полностью
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(get.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Осталось символов:", len(body))
+
+	// POST-запрос
+
+	fmt.Println()
+	fmt.Println("POST:", addr)
+
+	data := strings.NewReader("payload data")
+	req, err := http.NewRequest("POST", addr, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Статус ответа:", res.Status)
 }
