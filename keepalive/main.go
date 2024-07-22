1a79a54c4d9fb1dcb5a5d36544250b28c6343b53
Григорий Морозов <greg-frost@yandex.ru>
2024-07-22 18:49:43 +0300

    Learn keepalive

---
+++ keepalive/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fmt.Println(" \n[ ПОВТОРНОЕ ИСПОЛЬЗОВАНИЕ ]\n ")
+
+	// Транспорт
+	tr := &http.Transport{
+		// DisableKeepAlives: true,
+		// DisableCompression: true,
+		Dial: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).Dial,
+	}
+
+	// Клиент
+	client := &http.Client{Transport: tr}
+
+	times := 25
+	fmt.Println("Число повторов:", times)
+	fmt.Println()
+
+	// Запрос 1
+	var recieved int
+	start := time.Now()
+	for i := 0; i < times; i++ {
+		r, err := client.Get("https://go.dev")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		r.Body.Close()
+		recieved += len(o)
+	}
+	fmt.Println("Запрос 1:")
+	fmt.Println("Получено", recieved, "байт")
+	fmt.Println("Заняло", time.Now().Sub(start))
+	fmt.Println()
+
+	// Запрос 2
+	recieved = 0
+	start = time.Now()
+	for i := 0; i < times; i++ {
+		r, err := client.Get("http://example.com")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		r.Body.Close()
+		recieved += len(o)
+	}
+	fmt.Println("Запрос 1:")
+	fmt.Println("Получено", recieved, "байт")
+	fmt.Println("Заняло", time.Now().Sub(start))
+}
