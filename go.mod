c875e8523dadeadd61bba40ee052c416a4f70dca
Григорий Морозов <greg-frost@yandex.ru>
2024-07-25 19:42:24 +0300

    Learn protobuf

--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -55,7 +56,7 @@ require (
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/tools v0.23.0 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ protobuf/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	pb "golearn/protobuf/user"
+
+	"google.golang.org/protobuf/proto"
+)
+
+// Обработчик protobuf
+func handleProtobuf(w http.ResponseWriter, r *http.Request) {
+	u := &pb.User{
+		Name:  proto.String("Greg Frost"),
+		Id:    proto.Int32(100021),
+		Email: proto.String("greg-frost@yandex.ru"),
+	}
+
+	body, err := proto.Marshal(u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/x-protobuf")
+	w.Write(body)
+}
+
+func main() {
+	fmt.Println(" \n[ PROTOBUF ]\n ")
+
+	/* Сервер */
+
+	fmt.Println("Сервер:")
+	go func() {
+		http.HandleFunc("/", handleProtobuf)
+
+		fmt.Println("Ожидаю обновлений...")
+		fmt.Println("(на http://localhost:8080)")
+		log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	}()
+
+	time.Sleep(250 * time.Millisecond)
+	fmt.Println()
+
+	/* Клиент */
+
+	fmt.Println("Клиент:")
+	res, err := http.Get("http://localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var u pb.User
+	err = proto.Unmarshal(b, &u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Имя:", u.GetName())
+	fmt.Println("ID:", u.GetId())
+	fmt.Println("E-mail:", u.GetEmail())
+	fmt.Println()
+
+	fmt.Println("RAW:", strings.TrimSpace(string(b)))
+}
