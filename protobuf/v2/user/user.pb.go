8ea8d3209aa03f7e5cb4c49115f8420d1b52f3c9
Григорий Морозов <greg-frost@yandex.ru>
2024-07-30 15:06:38 +0300

    Fix protobuf

--- protobuf/v2/user/user.pb.go
+++ protobuf/v2/user/user.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.34.2
 // 	protoc        v4.24.4
-// source: user.proto
+// source: protobuf/v2/user/user.proto
 
 package __
 
@@ -33,7 +33,7 @@ type User struct {
 func (x *User) Reset() {
 	*x = User{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_user_proto_msgTypes[0]
+		mi := &file_protobuf_v2_user_user_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -46,7 +46,7 @@ func (x *User) String() string {
 func (*User) ProtoMessage() {}
 
 func (x *User) ProtoReflect() protoreflect.Message {
-	mi := &file_user_proto_msgTypes[0]
+	mi := &file_protobuf_v2_user_user_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -59,7 +59,7 @@ func (x *User) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use User.ProtoReflect.Descriptor instead.
 func (*User) Descriptor() ([]byte, []int) {
-	return file_user_proto_rawDescGZIP(), []int{0}
+	return file_protobuf_v2_user_user_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *User) GetName() string {
@@ -83,34 +83,35 @@ func (x *User) GetEmail() string {
 	return ""
 }
 
-var File_user_proto protoreflect.FileDescriptor
+var File_protobuf_v2_user_user_proto protoreflect.FileDescriptor
 
-var file_user_proto_rawDesc = []byte{
-	0x0a, 0x0a, 0x75, 0x73, 0x65, 0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x04, 0x75, 0x73,
-	0x65, 0x72, 0x22, 0x40, 0x0a, 0x04, 0x55, 0x73, 0x65, 0x72, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61,
-	0x6d, 0x65, 0x18, 0x01, 0x20, 0x02, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x0e,
-	0x0a, 0x02, 0x69, 0x64, 0x18, 0x02, 0x20, 0x02, 0x28, 0x05, 0x52, 0x02, 0x69, 0x64, 0x12, 0x14,
-	0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x65,
-	0x6d, 0x61, 0x69, 0x6c, 0x42, 0x03, 0x5a, 0x01, 0x2e,
+var file_protobuf_v2_user_user_proto_rawDesc = []byte{
+	0x0a, 0x1b, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x76, 0x32, 0x2f, 0x75, 0x73,
+	0x65, 0x72, 0x2f, 0x75, 0x73, 0x65, 0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x04, 0x75,
+	0x73, 0x65, 0x72, 0x22, 0x40, 0x0a, 0x04, 0x55, 0x73, 0x65, 0x72, 0x12, 0x12, 0x0a, 0x04, 0x6e,
+	0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x02, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12,
+	0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x02, 0x20, 0x02, 0x28, 0x05, 0x52, 0x02, 0x69, 0x64, 0x12,
+	0x14, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05,
+	0x65, 0x6d, 0x61, 0x69, 0x6c, 0x42, 0x03, 0x5a, 0x01, 0x2e,
 }
 
 var (
-	file_user_proto_rawDescOnce sync.Once
-	file_user_proto_rawDescData = file_user_proto_rawDesc
+	file_protobuf_v2_user_user_proto_rawDescOnce sync.Once
+	file_protobuf_v2_user_user_proto_rawDescData = file_protobuf_v2_user_user_proto_rawDesc
 )
 
-func file_user_proto_rawDescGZIP() []byte {
-	file_user_proto_rawDescOnce.Do(func() {
-		file_user_proto_rawDescData = protoimpl.X.CompressGZIP(file_user_proto_rawDescData)
+func file_protobuf_v2_user_user_proto_rawDescGZIP() []byte {
+	file_protobuf_v2_user_user_proto_rawDescOnce.Do(func() {
+		file_protobuf_v2_user_user_proto_rawDescData = protoimpl.X.CompressGZIP(file_protobuf_v2_user_user_proto_rawDescData)
 	})
-	return file_user_proto_rawDescData
+	return file_protobuf_v2_user_user_proto_rawDescData
 }
 
-var file_user_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_user_proto_goTypes = []any{
+var file_protobuf_v2_user_user_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_protobuf_v2_user_user_proto_goTypes = []any{
 	(*User)(nil), // 0: user.User
 }
-var file_user_proto_depIdxs = []int32{
+var file_protobuf_v2_user_user_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -118,13 +119,13 @@ var file_user_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_user_proto_init() }
-func file_user_proto_init() {
-	if File_user_proto != nil {
+func init() { file_protobuf_v2_user_user_proto_init() }
+func file_protobuf_v2_user_user_proto_init() {
+	if File_protobuf_v2_user_user_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_user_proto_msgTypes[0].Exporter = func(v any, i int) any {
+		file_protobuf_v2_user_user_proto_msgTypes[0].Exporter = func(v any, i int) any {
 			switch v := v.(*User); i {
 			case 0:
 				return &v.state
@@ -141,18 +142,18 @@ func file_user_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_user_proto_rawDesc,
+			RawDescriptor: file_protobuf_v2_user_user_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_user_proto_goTypes,
-		DependencyIndexes: file_user_proto_depIdxs,
-		MessageInfos:      file_user_proto_msgTypes,
+		GoTypes:           file_protobuf_v2_user_user_proto_goTypes,
+		DependencyIndexes: file_protobuf_v2_user_user_proto_depIdxs,
+		MessageInfos:      file_protobuf_v2_user_user_proto_msgTypes,
 	}.Build()
-	File_user_proto = out.File
-	file_user_proto_rawDesc = nil
-	file_user_proto_goTypes = nil
-	file_user_proto_depIdxs = nil
+	File_protobuf_v2_user_user_proto = out.File
+	file_protobuf_v2_user_user_proto_rawDesc = nil
+	file_protobuf_v2_user_user_proto_goTypes = nil
+	file_protobuf_v2_user_user_proto_depIdxs = nil
 }
