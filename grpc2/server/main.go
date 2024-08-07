5d0d91b00b5c1fcbdb68a90c84c3ea2cd2441cef
Григорий Морозов <greg-frost@yandex.ru>
2024-08-07 17:45:31 +0300

    Learn grpc2

---
+++ grpc2/server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "golearn/grpc2/protos/hello"
+
+	"google.golang.org/grpc"
+)
+
+var port = flag.Int("port", 8888, "Порт сервера")
+
+// Структура "сервер"
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// Общение через gRPC
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Получено: %v", in.GetName())
+	return &pb.HelloReply{Message: "Привет, " + in.GetName() + "!"}, nil
+}
+
+func main() {
+	fmt.Println(" \n[ GRPC2 (СЕРВЕР) ]\n ")
+
+	// Парсинг флагов
+	flag.Parse()
+
+	// Запуск сервера
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Запуск на", listener.Addr())
+	fmt.Println("(ожидаю обновлений...)")
+	fmt.Println()
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
+}
