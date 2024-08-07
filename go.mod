0e1a0b236b74d1e2ddb5695d2837cefc967a1313
Григорий Морозов <greg-frost@yandex.ru>
2024-08-07 19:51:13 +0300

    Fix grpc2

--- go.mod
+++ go.mod
@@ -56,8 +56,9 @@ require (
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/tools v0.23.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ grpc2/client/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	pb "golearn/grpc2/protos/hello"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8888", "Адрес сервера")
+	name = flag.String("name", "Greg Frost", "Имя для приветствия")
+)
+
+func main() {
+	fmt.Println(" \n[ GRPC2 (КЛИЕНТ) ]\n ")
+
+	// Парсинг флагов
+	flag.Parse()
+
+	// Соединение с сервером
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// Создание клиента
+	c := pb.NewGreeterClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// Вызов gRPC-метода SayHello
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Метод SayHello: %q\n", r.GetMessage())
+}
