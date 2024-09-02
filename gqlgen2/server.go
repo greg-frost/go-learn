ba56dec8bde97feea33b16d310b407ef12090523
Григорий Морозов <greg-frost@yandex.ru>
2024-09-02 17:21:46 +0300

    Learn gqlgen2

---
+++ gqlgen2/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golearn/gqlgen/graph"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	fmt.Println(" \n[ GRAPHQL-СЕРВЕР ]\n ")
+
+	// Порт
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	// Сервер
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
+		Resolvers: &graph.Resolver{},
+	}))
+
+	// Обработчики
+	http.Handle("/", playground.Handler("GraphQL-сервер", "/query"))
+	http.Handle("/query", srv)
+
+	// Запуск сервера
+	fmt.Println("Ожидаю обновлений...")
+	fmt.Printf("(на http://localhost:%s)\n", port)
+	log.Fatal(http.ListenAndServe("localhost:"+port, nil))
+}
