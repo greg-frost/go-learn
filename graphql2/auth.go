043fc6d7d2e829e0d48055624b6f8d495640e1f8
Григорий Морозов <greg-frost@yandex.ru>
2024-09-18 20:44:48 +0300

    Fix graphql2

---
+++ graphql2/auth.go
@@ -0,0 +1,59 @@
+package auth
+
+import (
+	"context"
+	"database/sql"
+	"net/http"
+)
+
+// Ключ пользователя в контексте
+var userCtxKey = &contextKey{"user"}
+
+// Структура "ключ в контексте"
+type contextKey struct {
+	name string
+}
+
+// Структура "пользователь"
+type User struct {
+	Name    string
+	IsAdmin bool
+}
+
+// Промежуточный слой (аутентификация)
+func Middleware(db *sql.DB) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			c, err := r.Cookie("auth-cookie")
+
+			// Пропуск авторизованных пользователей дальше
+			if err != nil || c == nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			// Получение ID пользователя из cookie
+			userId, err := validateAndGetUserID(c)
+			if err != nil {
+				http.Error(w, "Неправильный cookie", http.StatusForbidden)
+				return
+			}
+
+			// Получение пользователя из БД
+			user := getUserByID(db, userId)
+
+			// Добавление пользователя в контекст
+			ctx := context.WithValue(r.Context(), userCtxKey, user)
+
+			// Вызов следующего обработчика
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// Извлечение пользователя из контекста
+func ForContext(ctx context.Context) *User {
+	raw, _ := ctx.Value(userCtxKey).(*User)
+	return raw
+}
