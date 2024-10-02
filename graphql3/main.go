dba91f9b026048dc94b3ef1fabe3165ad67017a0
Григорий Морозов <greg-frost@yandex.ru>
2024-10-02 15:57:54 +0300

    Fix graphql3

--- graphql3/main.go
+++ graphql3/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,7 +12,12 @@ import (
 func main() {
 	fmt.Println(" \n[ GRAPHQL 3 (GRAPHQL-GO) ]\n ")
 
-	// Схема
+	/* Простая схема */
+
+	fmt.Println("Простая схема:")
+	fmt.Println()
+
+	// Поля
 	fields := graphql.Fields{
 		"hello": &graphql.Field{
 			Type: graphql.String,
@@ -23,14 +29,16 @@ func main() {
 
 	// Объект Query
 	Query := graphql.ObjectConfig{Name: "Query", Fields: fields}
+	// Конфигурация
 	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(Query)}
+	// Схема
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("не удалось создать новую схему: %v", err)
 	}
 
 	// Запрос
-	query := "{\n    hello\n}"
+	query := `{ hello }`
 	fmt.Printf("Запрос:\n%s\n\n", query)
 
 	// Выполнение
@@ -41,6 +49,53 @@ func main() {
 	}
 
 	// Ответ
-	response, _ := json.MarshalIndent(r, "", "    ")
-	fmt.Printf("Ответ:\n%s\n", response)
+	response, _ := json.MarshalIndent(r, "", "   ")
+	fmt.Printf("Ответ:\n%s\n\n", response)
+
+	/* Использование контекста */
+
+	fmt.Println("Использование контекста:")
+	fmt.Println()
+
+	// Имя аргумента
+	const arg = "key"
+
+	// Функция получения поля
+	fieldFromContext := func(p graphql.ResolveParams) (interface{}, error) {
+		return p.Context.Value(p.Args[arg]), nil
+	}
+
+	// Объект Query, конфигурация и схема
+	schema, err = graphql.NewSchema(graphql.SchemaConfig{
+		Query: graphql.NewObject(graphql.ObjectConfig{
+			Name: "Query",
+			Fields: graphql.Fields{
+				"value": &graphql.Field{
+					Type: graphql.String,
+					Args: graphql.FieldConfigArgument{
+						arg: &graphql.ArgumentConfig{Type: graphql.String},
+					},
+					Resolve: fieldFromContext,
+				},
+			},
+		}),
+	})
+
+	// Запрос
+	query = `{ value(` + arg + `: "Username") }`
+	fmt.Printf("Запрос:\n%s\n\n", query)
+
+	// Выполнение
+	r = graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+		Context:       context.WithValue(context.TODO(), "Username", "Greg Frost"),
+	})
+	if len(r.Errors) > 0 {
+		log.Fatalf("не удалось выполнить операцию: %+v", r.Errors)
+	}
+
+	// Ответ
+	response, _ = json.MarshalIndent(r, "", "   ")
+	fmt.Printf("Ответ:\n%s\n\n", response)
 }
