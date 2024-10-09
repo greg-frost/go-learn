eb66b2d0e249f91d74ada97e65786ebb9133a7c0
Григорий Морозов <greg-frost@yandex.ru>
2024-10-09 19:32:34 +0300

    Fix db

--- db/main.go
+++ db/main.go
@@ -9,8 +9,19 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Дескриптор БД
+var db *sql.DB
+
+// Структура "альбом"
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
-	fmt.Println(" \n[ БАЗЫ ДАННЫХ ]\n ")
+	fmt.Println(" \n[ БАЗА ДАННЫХ ]\n ")
 
 	/* Подключение */
 
@@ -52,7 +63,7 @@ func main() {
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-	// db := sql.OpenDB(connector)
+	// db = sql.OpenDB(connector)
 
 	defer db.Close()
 
@@ -61,4 +72,51 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Успешное подключение")
+	fmt.Println()
+
+	// Удаление старой таблицы
+	_, err = db.Exec("DROP TABLE IF EXISTS album")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Старая таблица удалена")
+
+	// Создание новой таблицы
+	_, err = db.Exec(`
+		CREATE TABLE album (
+			id INT AUTO_INCREMENT NOT NULL,
+			title VARCHAR(128) NOT NULL,
+			artist VARCHAR(255) NOT NULL,
+			price DECIMAL(5,2) NOT NULL,
+			PRIMARY KEY (id))
+		`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Новая таблица создана")
+
+	// Вставка записей
+	insert, err := db.Exec(`
+		INSERT INTO album
+			(title, artist, price)
+		VALUES
+			('Blue Train', 'John Coltrane', 56.99),
+			('Giant Steps', 'John Coltrane', 63.99),
+			('Jeru', 'Gerry Mulligan', 17.99),
+			('Jeru (remastered)', 'Gerry Mulligan', 19.99),
+			('Sarah Vaughan', 'Sarah Vaughan', 34.98)
+		`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	inserted, _ := insert.RowsAffected()
+	fmt.Printf("Добавлены записи (%d)\n", inserted)
+	fmt.Println()
+
+	// Убаление таблицы
+	_, err = db.Exec("DROP TABLE album")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Таблица удалена")
 }
