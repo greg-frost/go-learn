8e4adfc8b00ba92af40a39034e718000dd0bb7f7
Григорий Морозов <greg-frost@yandex.ru>
2024-11-06 19:21:10 +0300

    Fix migrate

--- go.mod
+++ go.mod
@@ -44,8 +44,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
@@ -61,6 +65,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/urfave/cli/v2 v2.27.2 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/mod v0.21.0 // indirect
--- migrate/main.go
+++ migrate/main.go
@@ -1,8 +1,62 @@
 package main
 
 import (
+	"database/sql"
+	"embed"
+	"errors"
 	"fmt"
+	"path/filepath"
+
+	"go-learn/base"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
+
+// Путь
+var path = base.Dir("migrate")
+
+// Структура "мигратор"
+type Migrator struct {
+	driver source.Driver
+}
+
+// Обязательное получение нового мигратора
+func MustNewMigrator(files embed.FS, dir string) *Migrator {
+	d, err := iofs.New(files, filepath.Join(path, dir))
+	if err != nil {
+		panic(err)
+	}
+	return &Migrator{driver: d}
+}
+
+// Применение миграций
+func (m *Migrator) ApplyMigrations(db *sql.DB) error {
+	// Получение драйвера БД
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("не удалось создать экземпляр БД: %v", err)
+	}
+
+	// Получение мигратора
+	migrator, err := migrate.NewWithInstance(
+		"migration_embeded_sql_files",
+		m.driver, "psql_db", driver,
+	)
+	if err != nil {
+		return fmt.Errorf("не удалось создать экземпляр мигратора: %v", err)
+	}
+	defer migrator.Close()
+
+	// Миграции
+	if err := migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("не удалось применить миграцию: %v", err)
+	}
+
+	return nil
+}
 
 func main() {
 	fmt.Println(" \n[ GO-MIGRATE ]\n ")
