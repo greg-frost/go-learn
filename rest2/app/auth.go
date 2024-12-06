55b9b3612100a3ec24735bee0781c2573e347012
Григорий Морозов <greg-frost@yandex.ru>
2024-12-06 17:32:42 +0300

    Fix rest2

--- rest2/app/auth.go
+++ rest2/app/auth.go
@@ -3,27 +3,26 @@ package app
 import (
 	"net/http"
 
-	"go-learn/rest2/utils"
+	u "go-learn/rest2/utils"
 )
 
 var jwtAuth = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		skipAuth := []string{"api/user/new", "api/user/login"}
-		requestPath := r.URL.Path
-		for _, skip := range skipAuth {
-			if skip == requestPath {
-				next.ServeHTTP(w, r)
-				return
-			}
+		skipAuth := map[string]bool{
+			"api/user/new":   true,
+			"api/user/login": true,
+		}
+		if skipAuth[r.URL.Path] {
+			next.ServeHTTP(w, r)
+			return
 		}
 
 		response := make(map[string]interface{})
 		tokenHeader := r.Header.Get("Authorization")
 		if tokenHeader == "" {
-			response = utils.Message(false, "Missing auth token")
+			response = u.Message(false, "Missing auth token")
 			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			utils.Respond(w, response)
+			u.Respond(w, response)
 			return
 		}
 	})
