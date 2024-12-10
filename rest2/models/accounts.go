fe9b1246fb754ad3d24b96b5449740aed5e7403a
Григорий Морозов <greg-frost@yandex.ru>
2024-12-10 20:16:05 +0300

    Fix rest2

--- rest2/models/accounts.go
+++ rest2/models/accounts.go
@@ -1,8 +1,28 @@
 package models
 
-import jwt "github.com/dgrijalva/jwt-go"
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+	"gorm.io/gorm"
+)
 
+// Структура "токен"
 type Token struct {
 	UserID uint
 	jwt.StandardClaims
+}
+
+// Структура "аккаунт"
+type Account struct {
+	gorm.Model
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Token    string `json:"token";sql:"-"`
+}
+
+// Структура "контакт"
+type Contact struct {
+	gorm.Model
+	Name   string `json:"name"`
+	Phone  string `json:"phone"`
+	UserID uint   `json:"user_id"`
 }
---
+++ rest2/models/database.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+)
+
+// Дескриптор БД
+var db = *gorm.DB
+
+func init() {
+	// Загрузка переменных окружения
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Данные для подключения к БД
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	database := os.Getenv("db_name")
+	host := os.Getenv("db_host")
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, username, password, database,
+	)
+
+	// Подключение к БД
+	db, err = gorm.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Миграции
+	db.Debug().AutoMigrate(&Account{}, &Contact{})
+}
+
+// Получение дескриптора БД
+func GetDB() *gorm.DB {
+	return db
+}
