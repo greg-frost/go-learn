48db7f7de9b5a711405de4f7e6440d7df9d532e8
Григорий Морозов <greg-frost@yandex.ru>
2024-12-17 18:13:39 +0300

    Fix rest2

---
+++ rest2/controllers/auth.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"go-learn/rest2/models"
+	u "go-learn/rest2/utils"
+)
+
+// Создание аккаунта
+var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
+	account := &models.Account{}
+	if err := json.NewDecoder(r.Body).Decode(account); err != nil {
+		u.Respond(w, u.Message(false, "Неправильный запрос"))
+	}
+
+	resp := account.Create()
+	u.Respond(w, resp)
+}
+
+// Аутентификация
+var Authenticate = func(w http.ResponseWriter, r *http.Request) {
+	account := &models.Account{}
+	if err := json.NewDecoder(r.Body).Decode(account); err != nil {
+		u.Respond(w, u.Message(false, "Неправильный запрос"))
+	}
+
+	resp := models.Login(account.Email, account.Password)
+	u.Respond(w, resp)
+}
