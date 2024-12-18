599caf014429b305ceb95bc2e07cb8f554a259ab
Григорий Морозов <greg-frost@yandex.ru>
2024-12-18 19:50:44 +0300

    Fix rest2

--- rest2/models/contacts.go
+++ rest2/models/contacts.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	u "go-learn/rest2/utils"
+	"log"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -10,4 +13,60 @@ type Contact struct {
 	Name   string `json:"name"`
 	Phone  string `json:"phone"`
 	UserID uint   `json:"user_id"`
+}
+
+// Валидация контакта
+func (c *Contact) Validate() (map[string]interface{}, bool) {
+	if c.Name == "" {
+		return u.Message(false, "Имя контакта необходимо"), false
+	}
+
+	if c.Phone == "" {
+		return u.Message(false, "Телефон контакта необходим"), false
+	}
+
+	if c.UserID < 0 {
+		return u.Message(false, "Пользователь не распознан"), false
+	}
+
+	return u.Message(true, "Валидация контакта пройдена"), true
+}
+
+// Создание контакта
+func (c *Contact) Create() map[string]interface{} {
+	if resp, ok := c.Validate(); !ok {
+		return resp
+	}
+
+	DB().Create(c)
+
+	resp := u.Message(true, "Контакт создан")
+	resp["contact"] = c
+	return resp
+}
+
+// Получение контакта
+func GetContact(id uint) *Contact {
+	contact := &Contact{}
+
+	err := DB().Table("contacts").Where("id=?", id).First(contact).Error
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return contact
+}
+
+// Получение контактов
+func GetContacts(user uint) []*Contact {
+	contacts := make([]*Contact, 0)
+
+	err := DB().Table("contacts").Where("user_id=?", user).Find(&contacts).Error
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return contacts
 }
