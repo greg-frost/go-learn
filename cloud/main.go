a6250baabe5e198807cab84f407ddbfe5d32232a
Григорий Морозов <greg-frost@yandex.ru>
2025-01-21 14:34:05 +0300

    Fix cloud

--- cloud/main.go
+++ cloud/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-// Хранилище пар-значений
-var store map[string]string
+// Хранилище пар ключ/значение
+var store = make(map[string]string)
 
 // Ошибка поиска ключа
 var ErrKeyNotFound = errors.New("ключ не найден")
@@ -32,6 +37,43 @@ func Delete(key string) error {
 	return nil
 }
 
+// Обработчик добавления значения
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
 func main() {
 	fmt.Println(" \n[ GO CLOUD ]\n ")
+
+	// Новый роутер
+	r := mux.NewRouter()
+
+	// Обработчики
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+
+	// Запуск сервера
+	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("(на http://localhost:8080)")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
