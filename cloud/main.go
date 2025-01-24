0e8535061829428092c725e89e632b38b7e1b59b
Григорий Морозов <greg-frost@yandex.ru>
2025-01-24 16:27:35 +0300

    Fix cloud

--- cloud/main.go
+++ cloud/main.go
@@ -101,7 +101,32 @@ func (l *FileTransactionLogger) Err() <-chan error {
 	return l.errors
 }
 
-// Конструктор регистратора транзакций
+// Размер буфера событий
+const EventsCapacity = 16
+
+// Запуск регистратора
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, EventsCapacity)
+	l.events = events
+
+	errors := make(chan error, 1)
+	l.errors = errors
+
+	go func() {
+		for e := range events {
+			l.lastSequence++
+
+			_, err := fmt.Fprintf(l.file, "%d\t%d\t%s\t%s\n",
+				l.lastSequence, e.EventType, e.Key, e.Value)
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+// Конструктор регистратора
 func NewFileTransactionLogger(filename string) (TransactionLogger, error) {
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
 	if err != nil {
