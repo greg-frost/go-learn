48116a1fc1ea3c373337e018bb2f1566c97465b4
Григорий Морозов <greg-frost@yandex.ru>
2025-03-25 19:56:19 +0300

    Fix rest3

--- rest3/pkg/client/mongodb/mongodb.go
+++ rest3/pkg/client/mongodb/mongodb.go
@@ -2,17 +2,42 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func NewClient(ctx context.Context, host, port, username, password, database, authDB string) (*mongo.Database, error) {
-	// var mongoDBURL string
-	// if username == "" && password == "" {
-	// 	mongoDBURL = "mongodb://%s:%s"
-	// } else {
-	// 	mongoDBURL = "mongodb://%s:%s@%s:%s"
-	// }
+func NewClient(ctx context.Context, host, port, user, pass, db, authDB string) (*mongo.Database, error) {
+	var mongodbURL string
+	var isAuth bool
+	if user == "" && pass == "" {
+		mongodbURL = fmt.Sprintf("mongodb://%s:%s", host, port)
+	} else {
+		isAuth = true
+		mongodbURL = fmt.Sprintf("mongodb://%s:%s@%s:%s", user, pass, host, port)
+	}
 
-	return nil, nil
+	clientOptions := options.Client().ApplyURI(mongodbURL)
+	if isAuth {
+		if authDB == "" {
+			authDB = db
+		}
+		clientOptions.SetAuth(options.Credential{
+			AuthSource: authDB,
+			Username:   user,
+			Password:   pass,
+		})
+	}
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось подключиться к mongodb: %w", err)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("ошибка пинга к mongodb: %w", err)
+	}
+
+	return client.Database(db), nil
 }
