b2717aa9a2c05843a422c581e14f41bd629bd4c4
Григорий Морозов <greg-frost@yandex.ru>
2025-03-26 20:50:13 +0300

    Fix rest3

--- rest3/pkg/client/mongodb/mongodb.go
+++ rest3/pkg/client/mongodb/mongodb.go
@@ -8,7 +8,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Конструктор клиента
 func NewClient(ctx context.Context, host, port, user, pass, db, authDB string) (*mongo.Database, error) {
+	// Строка подключения
 	var mongodbURL string
 	var isAuth bool
 	if user == "" && pass == "" {
@@ -18,11 +20,13 @@ func NewClient(ctx context.Context, host, port, user, pass, db, authDB string) (
 		mongodbURL = fmt.Sprintf("mongodb://%s:%s@%s:%s", user, pass, host, port)
 	}
 
+	// Опции
 	clientOptions := options.Client().ApplyURI(mongodbURL)
 	if isAuth {
 		if authDB == "" {
 			authDB = db
 		}
+		// Авторизация
 		clientOptions.SetAuth(options.Credential{
 			AuthSource: authDB,
 			Username:   user,
@@ -30,11 +34,13 @@ func NewClient(ctx context.Context, host, port, user, pass, db, authDB string) (
 		})
 	}
 
+	// Подключение
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		return nil, fmt.Errorf("не удалось подключиться к mongodb: %w", err)
 	}
 
+	// Пинг
 	if err = client.Ping(ctx, nil); err != nil {
 		return nil, fmt.Errorf("ошибка пинга к mongodb: %w", err)
 	}
