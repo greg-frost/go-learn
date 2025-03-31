8057ace34688758944c51ceef2dd7cc26bc05639
Григорий Морозов <greg-frost@yandex.ru>
2025-03-31 17:20:55 +0300

    Fix rest3

--- rest3/internal/user/db/mongodb.go
+++ rest3/internal/user/db/mongodb.go
@@ -2,23 +2,44 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"go-learn/rest3/internal/user"
 	"go-learn/rest3/pkg/logger"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Структура "база данных"
 type db struct {
+	collection *mongo.Collection
+	logger     *logger.Logger
 }
 
 // Конструктор хранилища
-func NewStogare(collection string, logger *logger.Logger) user.Storage {
-	return &db{}
+func NewStorage(database *mongo.Database, collection string, logger *logger.Logger) user.Storage {
+	return &db{
+		collection: database.Collection(collection),
+		logger:     logger,
+	}
 }
 
 // Создание пользователя
 func (d *db) Create(ctx context.Context, user user.User) (string, error) {
-	return "", nil
+	d.logger.Debug("Создание пользователя")
+	result, err := d.collection.InsertOne(ctx, user)
+	if err != nil {
+		return "", fmt.Errorf("не удалось создать пользователя: %w", err)
+	}
+
+	d.logger.Debug("Получение ObjectID пользователя")
+	oid, ok := result.InsertedID.(primitive.ObjectID)
+	if !ok {
+		d.logger.Trace(user)
+		return "", fmt.Errorf("не удалось получить ObjectID пользователя: %w", err)
+	}
+	return oid.Hex(), nil
 }
 
 // Поиск конкретного пользователя
