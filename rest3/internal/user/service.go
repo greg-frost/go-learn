32899c91b0dac1858f8d74ff740b660a38fd3616
Григорий Морозов <greg-frost@yandex.ru>
2025-04-01 20:35:00 +0300

    Fix rest3

---
+++ rest3/internal/user/service.go
@@ -0,0 +1,18 @@
+package user
+
+import (
+	"context"
+
+	"go-learn/rest3/pkg/logger"
+)
+
+// Структура "сервис"
+type Service struct {
+	storage Storage
+	logger  *logger.Logger
+}
+
+// Создание пользователя
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) (User, error) {
+	panic("not implemented")
+}
