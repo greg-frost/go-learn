1b773616ed498ed1c1733032b301a395a513bf14
Григорий Морозов <greg-frost@yandex.ru>
2025-06-18 20:16:35 +0300

    Fix clean

--- clean/internal/adapters/api/book/service.go
+++ clean/internal/adapters/api/book/service.go
@@ -7,6 +7,10 @@ import (
 )
 
 // Интерфейс "сервис"
+// Замечание:
+// Перенос интерфейса сюда, т.е. ближе к месту использования,
+// не позволяет возвращать интерфейс в самом сервисе,
+// т.к. это создает циклическую зависимость
 type Service interface {
 	GetBookByUUID(ctx context.Context, uuid string) (*book.Book, error)
 	GetAllBooks(ctx context.Context, limit, offset int) ([]*book.Book, error)
