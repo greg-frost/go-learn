8fa1e4ff2502b848741588202a46195d546ed425
Григорий Морозов <greg-frost@yandex.ru>
2025-06-25 19:54:44 +0300

    Learn singleton

---
+++ singleton/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Структура "синглтон"
+type singleton struct {
+	store map[string]string
+}
+
+// Экземпляр синглтона
+var instance *singleton
+
+// Получение единственного экземпляра
+func GetInstance() *singleton {
+	if instance == nil {
+		instance = &singleton{
+			store: make(map[string]string),
+		}
+	}
+	return instance
+}
+
+// Чтение значения
+func (s *singleton) Get(key string) (string, bool) {
+	value, ok := s.store[key]
+	return value, ok
+}
+
+// Запись значения
+func (s *singleton) Put(key, value string) {
+	s.store[key] = value
+}
+
+// Удаление значения
+func (s *singleton) Delete(key string) {
+	delete(s.store, key)
+}
+
+func main() {
+	fmt.Println(" \n[ СИНГЛТОН ]\n ")
+
+	// Первый синглтон
+	fmt.Println("Первый экземпляр")
+	singleton := GetInstance()
+
+	fmt.Println("Запись значения:")
+	singleton.Put("key", "value")
+	fmt.Println("key -> value")
+
+	fmt.Println("Чтение значения:")
+	value, ok := singleton.Get("key")
+	fmt.Printf("key = %s (%t)\n", value, ok)
+	fmt.Println()
+
+	// Второй синглтон
+	fmt.Println("Первый экземпляр")
+	other := GetInstance()
+
+	fmt.Println("Чтение значения:")
+	value, ok = other.Get("key")
+	fmt.Printf("key = %s (%t)\n", value, ok)
+}
