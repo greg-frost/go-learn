3f14bf4ab2b06e42fbb095f1fa3946005dae7f7f
Григорий Морозов <greg-frost@yandex.ru>
2025-07-11 01:17:54 +0300

    Fix builder

--- builder/main.go
+++ builder/main.go
@@ -52,6 +52,15 @@ func (cb computerBuilder) SSD(volume int) Builder {
 
 // Построение
 func (cb computerBuilder) Build() Computer {
+	if cb.cpu == 0 {
+		cb.cpu = 4.0
+	}
+	if cb.ram == 0 {
+		cb.ram = 24
+	}
+	if cb.ssd == 0 {
+		cb.ssd = 2000
+	}
 	return Computer{
 		CPU: cb.cpu,
 		RAM: cb.ram,
@@ -72,13 +81,13 @@ func main() {
 		computer1.CPU, computer1.RAM, computer1.SSD)
 
 	// Построение второго компьютера
-	computer2 := compBuilder.CPU(4.0).RAM(16).SSD(1000).Build()
+	computer2 := compBuilder.RAM(16).SSD(1000).Build()
 	fmt.Println("Компьютер 2")
 	fmt.Printf("CPU: %.1f GHz, RAM: %d GB, SSD: %d GB\n",
 		computer2.CPU, computer2.RAM, computer2.SSD)
 
 	// Построение третьего компьютера
-	computer3 := compBuilder.CPU(4.5).RAM(24).SSD(2000).Build()
+	computer3 := compBuilder.CPU(4.5).Build()
 	fmt.Println("Компьютер 3")
 	fmt.Printf("CPU: %.1f GHz, RAM: %d GB, SSD: %d GB\n",
 		computer3.CPU, computer3.RAM, computer3.SSD)
