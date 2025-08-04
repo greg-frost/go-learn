0dc670192d9e14f7ad1549f83b12b0eadaa34a23
Григорий Морозов <greg-frost@yandex.ru>
2025-08-04 03:18:53 +0300

    Fix iterator

--- iterator/main.go
+++ iterator/main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// Интерфейс "итератор"
+type Iterator interface {
+	Next() string
+	HasNext() bool
+}
+
 // Структура "массив"
 type Array struct {
 	values []string
@@ -24,12 +30,6 @@ func (a *Array) GetIterator() Iterator {
 	return &ArrayIterator{
 		values: a.values,
 	}
-}
-
-// Интерфейс "итератор"
-type Iterator interface {
-	Next() string
-	HasNext() bool
 }
 
 // Структура "итератор массива"
@@ -53,6 +53,63 @@ func (i *ArrayIterator) HasNext() bool {
 	return i.pos < len(i.values)
 }
 
+// Структура "список"
+type List struct {
+	head *Node
+}
+
+// Структура "узел"
+type Node struct {
+	value string
+	next  *Node
+}
+
+// Конструктор списка
+func NewList() *List {
+	return &List{
+		head: new(Node),
+	}
+}
+
+// Добавление элементов
+func (l *List) Add(values ...string) {
+	curr := l.head
+	for _, value := range values {
+		next := &Node{
+			value: value,
+		}
+		curr.next = next
+		curr = curr.next
+	}
+}
+
+// Получение итератора для списка
+func (l *List) GetIterator() Iterator {
+	return &ListIterator{
+		head: l.head.next,
+	}
+}
+
+// Структура "итератор списка"
+type ListIterator struct {
+	head *Node
+}
+
+// Получение следующего элемента
+func (i *ListIterator) Next() string {
+	if !i.HasNext() {
+		return ""
+	}
+	value := i.head.value
+	i.head = i.head.next
+	return value
+}
+
+// Проверка существования следующего элемента
+func (i *ListIterator) HasNext() bool {
+	return i.head.next != nil
+}
+
 // Печать итератора
 func Print(it Iterator) {
 	if it.HasNext() {
@@ -73,4 +130,11 @@ func main() {
 	iterator := array.GetIterator()
 	Print(iterator) // Будет напечатано
 	Print(iterator) // Будет проигнорировано
+	fmt.Println()
+
+	// Итератор списка
+	list := NewArray()
+	list.Add("Goodbye", "Cruel", "World")
+	fmt.Println("Список:")
+	Print(list.GetIterator())
 }
