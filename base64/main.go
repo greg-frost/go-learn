6f8198c4b69f4441e95cfc4823bc1ebde2e84b7c
Григорий Морозов <greg-frost@yandex.ru>
2025-09-08 17:52:21 +0300

    Fix base64

--- base64/main.go
+++ base64/main.go
@@ -14,20 +14,21 @@ func main() {
 	fmt.Println()
 
 	// Стандартное кодирование
-	sEnc := base64.StdEncoding.EncodeToString(data)
-	sDec, _ := base64.StdEncoding.DecodeString(sEnc)
-	fmt.Println("Encode:", sEnc)
-	fmt.Println("Decode:", string(sDec))
+	stdEnc := base64.StdEncoding.EncodeToString(data)
+	stdDec, _ := base64.StdEncoding.DecodeString(stdEnc)
+	fmt.Println("Encode:", stdEnc)
+	fmt.Println("Decode:", string(stdDec))
 	fmt.Println()
 
-	// URL кодирование
-	uEnc := base64.URLEncoding.EncodeToString(data)
-	uDec, _ := base64.URLEncoding.DecodeString(uEnc)
-	fmt.Println("URL Encode:", uEnc)
-	fmt.Println("URL Decode:", string(uDec))
+	// URL-кодирование
+	urlEnc := base64.URLEncoding.EncodeToString(data)
+	urlDec, _ := base64.URLEncoding.DecodeString(urlEnc)
+	fmt.Println("URL Encode:", urlEnc)
+	fmt.Println("URL Decode:", string(urlDec))
 	fmt.Println()
 
-	if string(data) == string(sDec) && string(data) == string(uDec) {
+	// Сравнение
+	if string(data) == string(stdDec) && string(data) == string(urlDec) {
 		fmt.Println("Раскодировано верно")
 	} else {
 		fmt.Println("Раскодировано неверно")
