a28fa6b2643f10be573c6e17b91c53275a7f9cf8
Григорий Морозов <greg-frost@yandex.ru>
2025-09-23 14:36:43 +0300

    Fix health

--- health/main.go
+++ health/main.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
-// Проверка доступности
+// Проверка жизнеспособности (доступности)
 func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+// Поверхностная проверка работоспособности
+func shallowHealthHandler(w http.ResponseWriter, r *http.Request) {
+	// Создание временного файла
+	tmp, err := ioutil.TempFile(os.TempDir(), "shallow-")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer os.Remove(tmp.Name())
+
+	// Проверка записи в файл
+	text := []byte("Проверка работоспособности")
+	if _, err := tmp.Write(text); err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	// Проверка закрытия файла
+	if err := tmp.Close(); err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
@@ -17,6 +46,7 @@ func main() {
 
 	// Обработчики
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/shallow", shallowHealthHandler)
 
 	// Запуск сервера
 	fmt.Println("Ожидаю обновлений...")
