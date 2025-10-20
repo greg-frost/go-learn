fae80d9c1f338f817d48bfa5dbacf9c87d93812f
Григорий Морозов <greg-frost@yandex.ru>
2025-10-20 18:39:13 +0300

    Fix genref

--- genref/main.go
+++ genref/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"time"
 )
 
 // Обнуление среза (статический тип)
@@ -34,9 +35,13 @@ func nullifySliceReflect(s interface{}) {
 func main() {
 	fmt.Println(" \n[ ДЖЕНЕРИКИ И РЕФЛЕКСИЯ ]\n ")
 
+	// Обнуление
+	fmt.Println("Обнуление среза")
+	fmt.Println("---------------")
+
 	// Статический тип
 	staticSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println("Статический тип:")
+	fmt.Println("Статический:")
 	fmt.Println(staticSlice)
 	nullifySliceStatic(staticSlice)
 	fmt.Println(staticSlice)
@@ -58,4 +63,34 @@ func main() {
 	fmt.Println(reflectSlice)
 	nullifySliceReflect(reflectSlice)
 	fmt.Println(reflectSlice)
+	fmt.Println()
+
+	// Сравнение
+	fmt.Println("Сравнение скорости")
+	fmt.Println("------------------")
+
+	// Параметры
+	const times = 1e5
+	var slice = make([]int, 1000)
+
+	// Статический тип
+	start := time.Now()
+	for i := 0; i < times; i++ {
+		nullifySliceStatic(slice)
+	}
+	fmt.Println("Статический:", time.Since(start))
+
+	// Дженерики
+	start = time.Now()
+	for i := 0; i < times; i++ {
+		nullifySliceGenerics(slice)
+	}
+	fmt.Println("Дженерики:", time.Since(start))
+
+	// Рефлексия
+	start = time.Now()
+	for i := 0; i < times; i++ {
+		nullifySliceReflect(slice)
+	}
+	fmt.Println("Рефлексия:", time.Since(start))
 }
