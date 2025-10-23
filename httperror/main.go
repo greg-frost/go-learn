e327fd641488a186c2e8de28f50dd5f16683bbe0
Григорий Морозов <greg-frost@yandex.ru>
2025-10-24 00:49:12 +0300

    Fix httperror

--- httperror/main.go
+++ httperror/main.go
@@ -89,7 +89,7 @@ func main() {
 	go func() {
 		http.HandleFunc("/", handleError)
 
-		fmt.Println("Ожидаю обновлений...")
+		fmt.Println("Ожидаю соединений...")
 		fmt.Println("(на http://localhost:8080)")
 		log.Fatal(http.ListenAndServe("localhost:8080", nil))
 	}()
