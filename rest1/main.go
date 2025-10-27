56ec8dbfea44779ed6774f5387ca8718b51c4c62
Григорий Морозов <greg-frost@yandex.ru>
2025-10-27 03:40:06 +0300

    Fix rest1

--- rest1/main.go
+++ rest1/main.go
@@ -86,7 +86,7 @@ func main() {
 	// http.ListenAndServe("localhost:8080", mux)
 
 	// Запуск сервера
-	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("Ожидаю соединений...")
 	fmt.Println("(на http://localhost:8080)")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
