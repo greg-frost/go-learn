16164c88940a6a0efe38d1cabf4dc4bcaadb2be4
Григорий Морозов <greg-frost@yandex.ru>
2025-10-28 10:34:07 +0300

    Fix server

--- server/main.go
+++ server/main.go
@@ -54,7 +54,7 @@ func main() {
 	http.HandleFunc("/headers/", headersResponse)
 
 	// Запуск сервера
-	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("Ожидаю соединений...")
 	fmt.Println("(на http://localhost:8080)")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
