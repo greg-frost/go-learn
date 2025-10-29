2107b11fb5544cbc5a9ddad5c463f4c1e548184b
Григорий Морозов <greg-frost@yandex.ru>
2025-10-29 18:53:48 +0300

    Fix walkroutes

--- walkroutes/main.go
+++ walkroutes/main.go
@@ -72,7 +72,7 @@ func main() {
 	}
 
 	// Запуск сервера
-	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("Ожидаю соединений...")
 	fmt.Println("(на http://localhost:8080)")
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
