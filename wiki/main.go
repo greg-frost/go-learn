3b8232e2d20001d97352a32d7a3f058d7cee0064
Григорий Морозов <greg-frost@yandex.ru>
2025-10-30 12:15:37 +0300

    Fix wiki

--- wiki/main.go
+++ wiki/main.go
@@ -106,7 +106,7 @@ func main() {
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 
 	// Запуск сервера
-	fmt.Println("Ожидаю обновлений...")
+	fmt.Println("Ожидаю соединений...")
 	fmt.Println("(на http://localhost:8080)")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
