2fedbacb10abc48fcb7dd6d7529992ecbe4f9d55
Григорий Морозов <greg-frost@yandex.ru>
2025-11-28 16:36:13 +0300

    Fix errors

--- errors/main.go
+++ errors/main.go
@@ -42,10 +42,8 @@ func run() error {
 // Генерация своей ошибки
 func myRun() error {
 	var e error
-
-	e = errors.New("НИЧИВО-НИ-ВЫШЛА!") // можно так
-	e = fmt.Errorf("НИЧИВО-НИ-ВЫШЛА!") // а можно и этак
-
+	e = errors.New("НИЧИВО-НИ-ВЫШЛА!") // Можно так, ...
+	e = fmt.Errorf("НИЧИВО-НИ-ВЫШЛА!") // ... а можно и этак
 	return e
 }
 
@@ -151,18 +149,15 @@ func continueWithPanic(i int) {
 			fmt.Println("ПАНИКА:", p)
 		}
 	}()
-
 	fmt.Println(30 / i)
 }
 
 func main() {
 	fmt.Println(" \n[ ОШИБКИ ]\n ")
 
-	/* Ошибки */
-
+	// Ошибки
 	fmt.Println("Кастомные ошибки:")
 	fmt.Println()
-
 	if err := run(); err != nil {
 		switch e := err.(type) {
 		case *MyError:
@@ -171,15 +166,12 @@ func main() {
 			fmt.Println("Неизвестная ошибка:", e)
 		}
 	}
-
 	if myErr := myRun(); myErr != nil {
 		fmt.Println(myErr)
 	}
-
 	fmt.Println()
 
-	/* Мультиобработка */
-
+	// Мультиобработка
 	fmt.Println("Мультиобработка:")
 	multi, err := multiErrors(2, 0)
 	if err != nil {
@@ -188,28 +180,23 @@ func main() {
 		}
 	}
 	fmt.Println(multi, err)
-
 	fmt.Println()
 
-	/* IS и AS */
-
+	// IS и AS
 	err = fileChecker("dont_exist.txt")
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			fmt.Println("ОШИБКА IS: файла не существует")
 		}
 	}
-
 	err = run()
 	var MyErr MyErrorI
 	if errors.As(err, &MyErr) {
 		fmt.Println("ОШИБКА AS: совпадение типов ошибок")
 	}
-
 	fmt.Println()
 
-	/* Проверка введенной строки */
-
+	// Проверка введенной строки
 	fmt.Printf("Введите строку (2 пробела, <20 символов, без чисел): ")
 	reader := bufio.NewReader(os.Stdin)
 	ret, err := reader.ReadString('\n')
@@ -221,25 +208,19 @@ func main() {
 	} else {
 		fmt.Println("Все ОК!")
 	}
-
 	fmt.Println()
 
-	/* Паника */
-
+	// Паника
 	fmt.Println("Обработка паники:")
 	for _, val := range []int{1, 2, 0, 6} {
 		continueWithPanic(val)
 	}
-
 	fmt.Println()
-
 	fmt.Println("Вызов паники:")
-
 	defer func() {
 		err := recover()
 		fmt.Println("Анонимная функция:", err)
 	}()
 	defer saveFromPanic()
-
 	panic("Паника!")
 }
