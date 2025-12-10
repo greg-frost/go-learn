8037fb89d353d49ae1cfcb139323ab93decf1134
Григорий Морозов <greg-frost@yandex.ru>
2025-12-10 08:31:38 +0300

    Fix format

--- format/main.go
+++ format/main.go
@@ -13,22 +13,17 @@ type Point struct {
 func main() {
 	fmt.Println(" \n[ ФОРМАТИРОВАНИЕ ]\n ")
 
-	/* Структура */
-
+	// Структура
 	p := Point{1, 2}
-
 	fmt.Println("Структура:")
-
 	fmt.Printf("struct1  [ %%v ]   %v\n", p)
 	fmt.Printf("struct2  [ %%+v ]  %+v\n", p)
 	fmt.Printf("struct3  [ %%#v ]  %#v\n", p)
 	fmt.Printf("type     [ %%T ]   %T\n", p)
-
-	/* Типы */
-
 	fmt.Println()
-	fmt.Println("Типы:")
 
+	// Типы
+	fmt.Println("Типы:")
 	fmt.Printf("bool     [ %%t ]   %t\n", true)
 	fmt.Printf("int      [ %%d ]   %d\n", 123)
 	fmt.Printf("bin      [ %%b ]   %b\n", 14)
@@ -37,27 +32,23 @@ func main() {
 	fmt.Printf("float1   [ %%f ]   %f\n", 78.9)
 	fmt.Printf("float2   [ %%e ]   %e\n", 123400000.0)
 	fmt.Printf("float3   [ %%E ]   %E\n", 123400000.0)
-	fmt.Printf("str1     [ %%s ]   %s\n", "\"string\"")
-	fmt.Printf("str2     [ %%q ]   %q\n", "\"string\"")
+	fmt.Printf("str1     [ %%s ]   %s\n", "string")
+	fmt.Printf("str2     [ %%q ]   %q\n", "string")
 	fmt.Printf("str3     [ %%x ]   %x\n", "string")
 	fmt.Printf("pointer  [ %%p ]   %p\n", &p)
-
-	/* Выравнивание */
-
 	fmt.Println()
-	fmt.Println("Выравнивание:")
 
+	// Выравнивание
+	fmt.Println("Выравнивание:")
 	fmt.Printf("right:   | %6d | %6d |\n", 12, 345)
 	fmt.Printf("float:   | %6.2f | %6.2f |\n", 1.2, 3.45)
 	fmt.Printf("left:    | %-6.2f | %-6.2f |\n", 1.2, 3.45)
 	fmt.Printf("right:   | %6s | %6s |\n", "foo", "bar")
 	fmt.Printf("left:    | %-6s | %-6s |\n", "foo", "bar")
-
-	/* Разное */
-
 	fmt.Println()
-	fmt.Println("Разное:")
 
+	// Разное
+	fmt.Println("Разное:")
 	fmt.Println(fmt.Sprintf("sprintf  [ %s ]", "string"))
 	fmt.Fprintf(os.Stderr, "stderr   [ %s  ]\n", "error")
 }
