6e2c3accbe66bc288230f1f062d35eedf5c38a1b
Григорий Морозов <greg-frost@yandex.ru>
2025-12-17 10:36:51 +0300

    Fix select

--- select/main.go
+++ select/main.go
@@ -8,7 +8,7 @@ import (
 // Пауза (сон)
 func sleep(sec int) {
 	select {
-	case <-time.After(time.Second * time.Duration(sec)):
+	case <-time.After(time.Duration(sec) * time.Second):
 		return
 	}
 }
@@ -19,34 +19,31 @@ func main() {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
-	/* Первый канал */
-
+	// Первый канал
 	go func() {
 		count := 1
 		for {
 			ch1 <- "From 1"
-			time.Sleep(time.Second * 2)
+			time.Sleep(2 * time.Second)
 
 			// Закрытие после пары записей
 			if count == 3 {
 				close(ch1)
-				break
+				return
 			}
 			count++
 		}
 	}()
 
-	/* Второй канал */
-
+	// Второй канал
 	go func() {
 		for {
 			ch2 <- "From 2"
-			time.Sleep(time.Second * 3)
+			time.Sleep(3 * time.Second)
 		}
 	}()
 
-	/* Выбор канала */
-
+	// Выбор канала
 	go func() {
 		for {
 			select {
@@ -68,15 +65,13 @@ func main() {
 		}
 	}()
 
-	/* Сон */
-
+	// Сон
 	s := 5
 	fmt.Print("*** Засыпаем на ", s, " сек... *** ")
 	sleep(s)
 	fmt.Print("*** Проснулись! *** ")
 
-	/* Ожидание ввода */
-
+	// Ожидание ввода
 	var input string
 	fmt.Scanln(&input)
 }
