83e9c2ffdd1660ae292fba59d5e789433f0ec35c
Григорий Морозов <greg-frost@yandex.ru>
2026-01-01 13:27:19 +0300

    Learn project

---
+++ project/cmd/app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(" \n[ СТРУКТУРА ПРОЕКТА ]\n ")
+
+	fmt.Println("Смотрите help.txt в каталогах,")
+	fmt.Println("чтобы узнать подробнее...")
+}
