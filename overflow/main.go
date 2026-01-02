95e2f3aa758001b0f1c643d16184deadf7d7f31c
Григорий Морозов <greg-frost@yandex.ru>
2026-01-02 12:45:54 +0300

    Fix overflow

--- overflow/main.go
+++ overflow/main.go
@@ -63,6 +63,85 @@ func MultInt(a, b int) int {
 	return res
 }
 
+// Попытка инкремента
+func TryIncInt(counter int) {
+	fmt.Printf("%d + 1 = ", counter)
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	res := IncInt(counter)
+	fmt.Println(res)
+}
+
+// Попытка сложения
+func TryAddInt(a, b int) {
+	fmt.Printf("%d + %d = ", a, b)
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	res := AddInt(a, b)
+	fmt.Println(res)
+}
+
+// Попытка умножения
+func TryMultInt(a, b int) {
+	fmt.Printf("%d * %d = ", a, b)
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	res := MultInt(a, b)
+	fmt.Println(res)
+}
+
 func main() {
 	fmt.Println(" \n[ ПЕРЕПОЛНЕНИЕ ]\n ")
+
+	full := math.MaxInt
+	half := full / 2
+	quart := half / 2
+
+	fmt.Println("Инкремент:")
+	TryIncInt(quart)
+	TryIncInt(half)
+	TryIncInt(full - 1)
+	TryIncInt(full)
+	fmt.Println()
+
+	fmt.Println("Сложение:")
+	TryAddInt(quart, quart)
+	TryAddInt(half, quart)
+	TryAddInt(half, half)
+	TryAddInt(half+1, half+1)
+	TryAddInt(full, half)
+	TryAddInt(full, full)
+	TryAddInt(full, 1)
+	fmt.Println()
+
+	fmt.Println("Умножение:")
+	TryMultInt(quart, 0)
+	TryMultInt(quart, 1)
+	TryMultInt(quart, 2)
+	TryMultInt(quart, 3)
+	TryMultInt(quart, 4)
+	TryMultInt(quart+1, 4)
+	TryMultInt(quart, 5)
+	TryMultInt(half, 2)
+	TryMultInt(half+1, 2)
+	TryMultInt(half, 3)
+	TryMultInt(full, 1)
+	TryMultInt(full, 2)
+	TryMultInt(full+1, 1)
+	TryMultInt(full+1, 2)
 }
