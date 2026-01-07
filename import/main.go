3a90c24bb10e7175a591d8639c385f50f23a9017
Григорий Морозов <greg-frost@yandex.ru>
2026-01-07 18:22:42 +0300

    Fix import

--- import/main.go
+++ import/main.go
@@ -10,35 +10,27 @@ import (
 func main() {
 	fmt.Println(" \n[ ИМПОРТ ]\n ")
 
-	/* Математические операции */
-
+	// Математические операции
 	var a, b int = 5, 7
-
 	fmt.Println("Сумма", a, "и", b, "равна", math.Sum(a, b))
 	fmt.Println("Произведение", a, "и", b, "равно", math.Prod(a, b))
 	fmt.Println("Произведение через сумму", a, "и", b, "равно", math.ProdBySum(a, b))
-
 	fmt.Println()
 
-	/* Операции со срезами */
-
+	// Операции со срезами
 	s := slice.Slice{1, 2, 3}
 	fmt.Println("Срез:", s)
-
 	fmt.Println("Сумма среза:", slice.SumSlice(s))
-
 	slice.MapSlice(s, func(i slice.Element) slice.Element {
 		return i * 3
 	})
 	fmt.Println("Срез, умноженный на три:", s)
-
 	fmt.Println("Свертка слайса сложением:",
 		slice.FoldSlice(s,
 			func(x slice.Element, y slice.Element) slice.Element {
 				return x + y
 			},
 			0))
-
 	fmt.Println("Свертка слайса умножением:",
 		slice.FoldSlice(s,
 			func(x slice.Element, y slice.Element) slice.Element {
