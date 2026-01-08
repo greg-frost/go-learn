b6a1bcb0211447f03d4a1e3eac825a0c0b642988
Григорий Морозов <greg-frost@yandex.ru>
2026-01-09 00:11:42 +0300

    Fix mutex

--- mutex/main.go
+++ mutex/main.go
@@ -15,13 +15,13 @@ func main() {
 	for i := 0; i < 10; i++ {
 		go func(i int) {
 			m.Lock()
-			//defer m.Unlock() // Можно так...
+			//defer m.Unlock() // Можно так ...
 
 			fmt.Print(i, " start ")
 			time.Sleep(time.Second)
 			fmt.Print(i, " end ")
 
-			m.Unlock() // ... Или так
+			m.Unlock() // ... или так
 		}(i)
 	}
 
