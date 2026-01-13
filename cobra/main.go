d0f0744f8aa234d0bcfcd91e0bae09129d71974b
Григорий Морозов <greg-frost@yandex.ru>
2026-01-13 12:03:23 +0300

    Fix cobra

--- cobra/main.go
+++ cobra/main.go
@@ -14,9 +14,17 @@ var pBool bool
 
 // Главная команда
 var rootCmd = &cobra.Command{
-	Use:  "flags",
-	Long: "Пример команды флагов для Cobra",
-	Run:  flagsFunc,
+	Use:   "cobra",
+	Short: "Главная",
+	Long:  "Главная команда Cobra",
+}
+
+// Команда флагов
+var flagsCmd = &cobra.Command{
+	Use:   "flags",
+	Short: "Флаги",
+	Long:  "Команда флагов Cobra",
+	Run:   flagsFunc,
 }
 
 // Обработчик флагов
@@ -29,9 +37,12 @@ func flagsFunc(cmd *cobra.Command, args []string) {
 
 func init() {
 	// Настройка флагов
-	rootCmd.Flags().StringVarP(&pStr, "string", "s", "empty", "строковый флаг")
-	rootCmd.Flags().IntVarP(&pInt, "number", "n", 0, "числовой флаг")
-	rootCmd.Flags().BoolVarP(&pBool, "boolean", "b", false, "логический флаг")
+	flagsCmd.Flags().StringVarP(&pStr, "string", "s", "empty", "строковый флаг")
+	flagsCmd.Flags().IntVarP(&pInt, "number", "n", 0, "числовой флаг")
+	flagsCmd.Flags().BoolVarP(&pBool, "boolean", "b", false, "логический флаг")
+
+	// Настройка команд
+	rootCmd.AddCommand(flagsCmd)
 }
 
 func main() {
