6fcb8bba5a39ad6335b18ee3aceb9260b18db618
Григорий Морозов <greg-frost@yandex.ru>
2026-01-26 16:15:20 +0300

    Fix featflag

--- featflag/main.go
+++ featflag/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/spf13/viper"
 )
@@ -9,11 +10,40 @@ import (
 // Включение функционала вручную
 // const useNewFeature = true
 
-// Флаг использования функционала
+// Флаг использования функционала (по переменной окружения)
+// func FeatureEnabled(feature string) bool {
+// 	state := strings.ToLower(os.Getenv(feature))
+// 	return state == "true" || state == "on" || strings.HasPrefix(state, "enable")
+// }
+
+// Тип "включение функционала"
+type Enabled func(int) bool
+
+// Функции включения функционала
+var enabledFunctions = map[string]Enabled{
+	"use_new_feature": enabledByChance,
+}
+
+// Включение с определенной вероятностью
+func enabledByChance(chance int) bool {
+	return rand.Intn(100) < chance
+}
+
+// Флаг использования функционала (с определенной вероятностью)
 func FeatureEnabled(feature string) bool {
-	// state := strings.ToLower(os.Getenv(feature))
-	// return state == "true" || state == "on" || strings.HasPrefix(state, "enable")
-	return viper.GetBool(feature)
+	// Если задан флаг - использовать его
+	if viper.IsSet(feature) {
+		return viper.GetBool(feature)
+	}
+
+	// Поиск функции включения функционала
+	enabledFunc, ok := enabledFunctions[feature]
+	if !ok {
+		return false
+	}
+
+	// Вызов функции включения функционала
+	return enabledFunc(25)
 }
 
 // Старый функционал
