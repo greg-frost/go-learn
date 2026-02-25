3d622e76b040eca641f1317942d262d83cc051a7
Григорий Морозов <greg-frost@yandex.ru>
2026-02-25 05:26:09 +0300

    Fix url

--- url/main.go
+++ url/main.go
@@ -19,26 +19,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	/* Компоненты URL */
-
+	// Компоненты URL
 	fmt.Println("Схема (протокол):", u.Scheme)
-
 	fmt.Println("Пользователь:", u.User)
 	fmt.Println("Логин:", u.User.Username())
-	p, _ := u.User.Password()
-	fmt.Println("Пароль:", p)
-
+	password, _ := u.User.Password()
+	fmt.Println("Пароль:", password)
 	fmt.Println("Адрес:", u.Host)
 	host, port, _ := net.SplitHostPort(u.Host)
 	fmt.Println("Хост:", host)
 	fmt.Println("Порт:", port)
-
 	fmt.Println("Путь:", u.Path)
-
 	fmt.Println("Строка запроса:", u.RawQuery)
 	m, _ := url.ParseQuery(u.RawQuery)
 	fmt.Println("Карта параметров:", m)
 	fmt.Println("Параметр key:", m["key"][0])
-
 	fmt.Println("Якорь:", u.Fragment)
 }
