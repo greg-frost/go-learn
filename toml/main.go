a1966dda1f24c753238996fe757b10a7cdf96ba1
Григорий Морозов <greg-frost@yandex.ru>
2026-02-27 12:09:11 +0300

    Fix toml

--- toml/main.go
+++ toml/main.go
@@ -50,24 +50,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	/* Вывод */
-
+	// Печать
 	fmt.Printf("Title: %v (%T)\n", config.Title, config.Title)
 	fmt.Println()
-
 	fmt.Println("User:")
 	fmt.Printf("   ID: %v (%T)\n", config.User.Id, config.User.Id)
 	fmt.Printf("   Rate: %v (%T)\n", config.User.Rate, config.User.Rate)
 	fmt.Printf("   Name: %v (%T)\n", config.User.Name, config.User.Name)
 	fmt.Printf("   Birthday: %v (%T)\n", config.User.Birthday, config.User.Birthday)
 	fmt.Println()
-
 	fmt.Println("Db:")
 	fmt.Printf("   Server: %v (%T)\n", config.Db.Server, config.Db.Server)
 	fmt.Printf("   Ports: %v (%T)\n", config.Db.Ports, config.Db.Ports)
 	fmt.Printf("   Enabled: %v (%T)\n", config.Db.Enabled, config.Db.Enabled)
 	fmt.Println()
-
 	fmt.Println("Servers:")
 	fmt.Printf("   Alpha:\n")
 	fmt.Printf("      IP: %v (%T)\n", config.Servers.Alpha.Ip, config.Servers.Alpha.Ip)
@@ -76,7 +72,6 @@ func main() {
 	fmt.Printf("      IP: %v (%T)\n", config.Servers.Omega.Ip, config.Servers.Omega.Ip)
 	fmt.Printf("      DC: %v (%T)\n", config.Servers.Omega.Dc, config.Servers.Omega.Dc)
 	fmt.Println()
-
 	fmt.Println("Clients:")
 	fmt.Printf("   Data: %v (%T)\n", config.Clients.Data, config.Clients.Data)
 	fmt.Printf("   Hosts: %v (%T)\n", config.Clients.Hosts, config.Clients.Hosts)
