ec7acc36589af71439ac6f742a8a033b2982261b
Григорий Морозов <greg-frost@yandex.ru>
2026-03-04 13:40:56 +0300

    Fix reflect

--- reflect/main.go
+++ reflect/main.go
@@ -10,8 +10,8 @@ import (
 type MyType struct {
 	IntField int
 	StrField string
-	PtrField *float64 // ок, если будет не указатель
-	//SliceField []int // недопустимо для сравнения, но ok для рефлексии
+	PtrField *float64 // ОК, если будет не указатель
+	//SliceField []int // Недопустимо для сравнения, но ОК для рефлексии
 }
 
 // Сравнение пользовательских типов
@@ -22,6 +22,61 @@ func (mt1 MyType) IsEqual(mt2 MyType) bool {
 // Глубокое сравнение пользовательских типов
 func (mt1 MyType) IsDeepEqual(mt2 MyType) bool {
 	return reflect.DeepEqual(mt1, mt2)
+}
+
+// Структура "пользовательская структура"
+type MyStruct struct {
+	A int
+	B string
+	C bool
+}
+
+// Изменение поля структуры
+func ChangeFieldByName(v interface{}, fname string, newval int) {
+	val := reflect.ValueOf(v)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Struct {
+		return
+	}
+
+	field := val.FieldByName(fname)
+	if field.IsValid() {
+		if field.CanSet() { // OK для экспортируемых полей, переданных по указателю
+			switch field.Kind() {
+			case reflect.Int:
+				field.SetInt(int64(newval))
+			case reflect.String:
+				field.SetString(strconv.Itoa(newval))
+			}
+		}
+	}
+}
+
+// Печать структуры
+func PrintStruct(v interface{}) {
+	val := reflect.ValueOf(v)
+
+	switch val.Kind() {
+	case reflect.Ptr:
+		if val.Elem().Kind() != reflect.Struct {
+			fmt.Printf("Pointer to %v : %v", val.Elem().Type(), val.Elem())
+			return
+		}
+		val = val.Elem() // Если указатель на структуру, берем ее
+
+	case reflect.Struct: // Работаем со структурой
+	default:
+		fmt.Printf("%v : %v", val.Type(), val)
+		return
+	}
+
+	fmt.Printf("Структура %v (полей - %d):\n", val.Type(), val.NumField())
+	for fieldIndex := 0; fieldIndex < val.NumField(); fieldIndex++ {
+		field := val.Field(fieldIndex)
+		fmt.Printf("\t%v %v: %v\n", val.Type().Field(fieldIndex).Name, field.Type(), field)
+	}
 }
 
 // Структура "нулевой пользовательский тип"
@@ -43,162 +98,74 @@ func IsNil(obj interface{}) bool {
 		return false
 	}
 
-	if objValue.IsNil() {
-		return true
-	}
-
-	return false
-}
-
-// Структура "пользовательская структура"
-type MyStruct struct {
-	A int
-	B string
-	C bool
-}
-
-// Печать структуры
-func PrintStruct(v interface{}) {
-	val := reflect.ValueOf(v)
-
-	switch val.Kind() {
-	case reflect.Ptr:
-		if val.Elem().Kind() != reflect.Struct {
-			fmt.Printf("Pointer to %v : %v", val.Elem().Type(), val.Elem())
-			return
-		}
-		val = val.Elem() // если указатель на структуру, берем ее
-
-	case reflect.Struct: // работаем со структурой
-	default:
-		fmt.Printf("%v : %v", val.Type(), val)
-		return
-	}
-
-	fmt.Printf("Структура %v (полей - %d):\n", val.Type(), val.NumField())
-	for fieldIndex := 0; fieldIndex < val.NumField(); fieldIndex++ {
-		field := val.Field(fieldIndex)
-		fmt.Printf("\t%v %v: %v\n", val.Type().Field(fieldIndex).Name, field.Type(), field)
-	}
-}
-
-// Изменение поля структуры
-func ChangeFieldByName(v interface{}, fname string, newval int) {
-	val := reflect.ValueOf(v)
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-	if val.Kind() != reflect.Struct {
-		return
-	}
-
-	field := val.FieldByName(fname)
-	if field.IsValid() {
-		if field.CanSet() { // ок для экспортируемых полей, переданных по указателю
-			switch field.Kind() {
-			case reflect.Int:
-				field.SetInt(int64(newval))
-			case reflect.String:
-				field.SetString(strconv.Itoa(newval))
-			}
-		}
-	}
+	return objValue.IsNil()
 }
 
 func main() {
 	fmt.Println(" \n[ РЕФЛЕКСИЯ ]\n ")
 
+	// Равенство
 	floatValue1, floatValue2 := 10.0, 10.0
 	a := MyType{IntField: 1, StrField: "str", PtrField: &floatValue1}
 	b := MyType{IntField: 1, StrField: "str", PtrField: &floatValue2}
-
 	fmt.Printf("Обычное равенство a и b: %v\n", a.IsEqual(b))
 	fmt.Printf("Глубокое равенство a и b: %v\n", a.IsDeepEqual(b))
-
 	fmt.Println()
 
-	/* Type и Kind */
-
+	// Type и Kind
 	fmt.Println("Type и Kind:")
 	fmt.Println()
-
 	var Bool *bool
-	fmt.Printf(
-		"< *bool >\nType: %v\nKind: %v\n\n",
-		reflect.ValueOf(Bool).Type(), reflect.ValueOf(Bool).Kind(),
-	)
-
+	vb := reflect.ValueOf(Bool)
+	fmt.Printf("< *bool >\nType: %v\nKind: %v\n\n", vb.Type(), vb.Kind())
 	var Float float32
-	fmt.Printf(
-		"< float32 >\nType: %v\nKind: %v\n\n",
-		reflect.ValueOf(Float).Type(), reflect.ValueOf(Float).Kind(),
-	)
-
+	vf := reflect.ValueOf(Float)
+	fmt.Printf("< float32 >\nType: %v\nKind: %v\n\n", vf.Type(), vf.Kind())
 	var Map map[string]int
-	fmt.Printf(
-		"< map[string]int >\nKind: %v\nType: %v\n\n",
-		reflect.ValueOf(Map).Type(), reflect.ValueOf(Map).Kind(),
-	)
-
+	vm := reflect.ValueOf(Map)
+	fmt.Printf("< map[string]int >\nKind: %v\nType: %v\n\n", vm.Type(), vm.Kind())
 	Struct := struct{ Value int }{}
-	fmt.Printf(
-		"< struct{Value int} >\nKind: %v\nType: %v\n\n",
-		reflect.ValueOf(Struct).Type(), reflect.ValueOf(Struct).Kind(),
-	)
+	vs := reflect.ValueOf(Struct)
+	fmt.Printf("< struct{Value int} >\nKind: %v\nType: %v\n\n", vs.Type(), vs.Kind())
 
-	/* Перебор полей структуры */
-
+	// Перебор полей структуры
 	fmt.Println("Печать структур:")
 	fmt.Println()
-
 	s := &MyStruct{
 		A: 3,
 		B: "some",
 		C: false,
 	}
-
 	ChangeFieldByName(s, "A", 5)
-
 	PrintStruct(s)
 	PrintStruct(struct {
 		E int
 		C string
 	}{10, "text"})
-
 	fmt.Println()
 
-	/* Разное */
-
+	// Разное
 	fmt.Println("Разное:")
 	fmt.Println()
-
 	var t *MyNilType
 	fmt.Printf("%v - это naive nil? %v\n", reflect.TypeOf(t), IsNaiveNil(t))
 	fmt.Printf("%v - это nil? %v\n", reflect.TypeOf(t), IsNil(t))
-
 	fmt.Println()
-
 	varInt := 100
 	varIntValue := reflect.ValueOf(varInt)
 	fmt.Println("100 - это zero?", varIntValue.IsZero())
 	fmt.Println("Значение (100) =", varIntValue.Int())
-
 	fmt.Println()
-
 	var varPtr *int
 	varPtrValue := reflect.ValueOf(varPtr)
 	fmt.Println("*int - это nil?", varPtrValue.IsNil())
 	fmt.Println("*int - это zero?", varPtrValue.IsZero())
-
 	fmt.Println()
-
 	var varBool *bool
 	fmt.Println("*bool - это nil?", reflect.ValueOf(varBool).IsNil())
-
 	trueVal := true
 	reflect.ValueOf(&varBool).Elem().Set(reflect.ValueOf(&trueVal))
 	fmt.Println("(присвоено значение true)")
-
 	fmt.Println("bool (true) - это nil?", reflect.ValueOf(varBool).IsNil())
 	fmt.Println("bool (true) =", reflect.ValueOf(varBool).Elem().Bool()) // вывод через рефлексию
 	//fmt.Println("bool (true) =", *varBool) // или обычным образом
