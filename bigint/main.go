70f7e89c02caaec35d56b492a35df202fd6a7679
Григорий Морозов <greg-frost@yandex.ru>
2026-03-09 09:25:26 +0300

    Fix bigint

--- bigint/main.go
+++ bigint/main.go
@@ -6,13 +6,12 @@ import (
 )
 
 // Число Фибоначчи
-func fib(n int) *big.Int {
+func Fib(n int) *big.Int {
 	if n < 2 {
 		return big.NewInt(int64(n))
 	}
 
 	a, b := big.NewInt(0), big.NewInt(1)
-
 	for n--; n > 0; n-- {
 		a.Add(a, b)
 		a, b = b, a
@@ -25,11 +24,11 @@ func main() {
 	fmt.Println(" \n[ BIG INT ]\n ")
 
 	size := 1000
-	fmt.Printf("Число Фибоначчи (%d):\n\n%v\n\n", size, fib(size))
+	fmt.Printf("Число Фибоначчи (%d):\n\n%v\n\n", size, Fib(size))
 
 	size = 5000
-	fmt.Printf("Число Фибоначчи (%d):\n\n%v\n\n", size, fib(size))
+	fmt.Printf("Число Фибоначчи (%d):\n\n%v\n\n", size, Fib(size))
 
 	size = 10000
-	fmt.Printf("Число Фибоначчи (%d):\n\n%v\n", size, fib(size))
+	fmt.Printf("Число Фибоначчи (%d):\n\n%v\n", size, Fib(size))
 }
