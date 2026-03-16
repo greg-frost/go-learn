998a5c071302e5c5c2d3d8176090d487053dc836
Григорий Морозов <greg-frost@yandex.ru>
2026-03-17 00:32:36 +0300

    Fix zap

--- zap/main.go
+++ zap/main.go
@@ -11,16 +11,29 @@ import (
 func main() {
 	fmt.Println(" \n[ ЛОГГЕР ZAP ]\n ")
 
-	// Инициализация
+	// Стандартный логгер
+	// (типизированный, строгий, быстрый)
 	logger, err := zap.NewProduction()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Использование
+	fmt.Println("Стандартный:")
 	logger.Info("не удалось получить URL",
 		zap.String("url", "example.com"),
 		zap.Int("attempt", 3),
 		zap.Duration("backoff", time.Second),
+	)
+	fmt.Println()
+
+	// Расширенный логгер
+	// (удобный, "сахарный", медленный)
+	sugar := logger.Sugar()
+
+	fmt.Println("Расширенный:")
+	sugar.Infow("не удалось получить URL",
+		"url", "example.com",
+		"attempt", 3,
+		"backoff", time.Second,
 	)
 }
