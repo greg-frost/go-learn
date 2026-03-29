b460dc0778c0c0bef29e2c93e701aa504f519cba
Григорий Морозов <greg-frost@yandex.ru>
2026-03-30 00:18:17 +0300

    Fix syslog

--- syslog/main.go
+++ syslog/main.go
@@ -10,7 +10,6 @@ func main() {
 	// Стандартный
 	fmt.Println("Стандартный логгер:")
 	fmt.Println("(не работает в Windows)")
-
 	// priority := syslog.LOG_LOCAL3 | syslog.LOG_NOTICE
 	// flags := log.LstdFlags | log.Lshortfile
 	// logger, err := syslog.NewLogger(priority, flags)
@@ -19,13 +18,11 @@ func main() {
 	// }
 	// logger.Printf("Hello\n")
 	// logger.Println("World")
-
 	fmt.Println()
 
 	// Уровневый
 	fmt.Println("Уровневый логгер:")
 	fmt.Println("(не работает в Windows)")
-
 	// logger, err = syslog.New(syslog.LOG_LOCAL3, "syslog")
 	// if err != nil {
 	// 	log.Fatal(err)
