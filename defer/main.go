5b312b829c58937c1b30e8a7dff7453aa5de20cb
Григорий Морозов <greg-frost@yandex.ru>
2026-04-07 14:50:58 +0300

    Fix defer

--- defer/main.go
+++ defer/main.go
@@ -4,16 +4,41 @@ import (
 	"fmt"
 )
 
+// Структура "структура"
+type Struct struct {
+	state bool
+}
+
+// Печать по значению
+func (s Struct) PrintByValue() {
+	fmt.Println("По значению:", s.state)
+}
+
+// Печать по указателю
+func (s *Struct) PrintByPointer() {
+	fmt.Println("По указателю:", s.state)
+}
+
 func main() {
 	fmt.Println(" \n[ DEFER ]\n ")
 
 	// Вычисление аргументов
 	var variable, closure bool
 	defer func(local bool) {
+		fmt.Println()
 		fmt.Println("Вычисление аргументов")
 		fmt.Println("Локальное:", local)
 		fmt.Println("Замыкание:", closure)
 	}(variable)
 	variable = true // Не увидит изменение
 	closure = true  // Увидит изменение
+
+	// Значение и указатель
+	fmt.Println("Значение и указатель")
+	p := &Struct{}
+	defer p.PrintByPointer()
+	p.state = true
+	v := Struct{}
+	defer v.PrintByValue()
+	v.state = true
 }
