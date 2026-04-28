d7ae608b39a0e9fd6debf32a34f0ae932f78f31b
Григорий Морозов <greg-frost@yandex.ru>
2026-04-29 00:36:45 +0300

    Fix float

--- float/main.go
+++ float/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Менее точная функция сложения
-func sumLessPrecise(n int) float64 {
+func SumLessPrecise(n int) float64 {
 	res := 10_000.
 	for i := 0; i < n; i++ {
 		res += 1.0001
@@ -15,7 +15,7 @@ func sumLessPrecise(n int) float64 {
 }
 
 // Более точная функция сложения
-func sumMorePrecise(n int) float64 {
+func SumMorePrecise(n int) float64 {
 	var res float64
 	for i := 0; i < n; i++ {
 		res += 1.0001
@@ -24,17 +24,17 @@ func sumMorePrecise(n int) float64 {
 }
 
 // Менее точная функция умножения
-func prodLessPrecise(a, b, c float64) float64 {
+func ProdLessPrecise(a, b, c float64) float64 {
 	return a * (b + c)
 }
 
 // Более точная функция умножения
-func prodMorePrecise(a, b, c float64) float64 {
+func ProdMorePrecise(a, b, c float64) float64 {
 	return a*b + a*c
 }
 
 // Сравнение чисел float
-func compareFloat(a, b, precise float64) bool {
+func CompareFloat(a, b, precise float64) bool {
 	return math.Abs(a-b) < precise
 }
 
@@ -45,12 +45,12 @@ func main() {
 	fmt.Println("Точность сложения:")
 	size := 1000
 	preciseSum := 11000.1
-	sum1 := sumLessPrecise(size)
-	sum2 := sumMorePrecise(size)
+	sum1 := SumLessPrecise(size)
+	sum2 := SumMorePrecise(size)
 	fmt.Printf("10000 + sum = %v (%v)\n", sum1, preciseSum-sum1)
 	fmt.Printf("sum + 10000 = %v (%v) [ точнее ]\n", sum2, preciseSum-sum2)
 	fmt.Println("Равенство:", sum1 == sum2)
-	fmt.Println("Сравнение:", compareFloat(sum1, sum2, 1e-6))
+	fmt.Println("Сравнение:", CompareFloat(sum1, sum2, 1e-6))
 	fmt.Println()
 
 	// Сравнение точности при умножении
@@ -59,10 +59,10 @@ func main() {
 	b := 1.0001
 	c := 1.0002
 	preciseProd := 200030.002
-	prod1 := prodLessPrecise(a, b, c)
-	prod2 := prodMorePrecise(a, b, c)
+	prod1 := ProdLessPrecise(a, b, c)
+	prod2 := ProdMorePrecise(a, b, c)
 	fmt.Printf("a * (b+c) = %v (%v)\n", prod1, prod1-preciseProd)
 	fmt.Printf("a*b + a*c = %v (%v) [ точнее ]\n", prod2, prod2-preciseProd)
 	fmt.Println("Равенство:", prod1 == prod2)
-	fmt.Println("Сравнение:", compareFloat(prod1, prod2, 1e-6))
+	fmt.Println("Сравнение:", CompareFloat(prod1, prod2, 1e-6))
 }
