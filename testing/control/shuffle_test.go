4f1ffa28d3407fba48b8adbaea5fe1af3ba93a73
Григорий Морозов <greg-frost@yandex.ru>
2026-05-13 17:18:11 +0300

    Fix testing

---
+++ testing/control/shuffle_test.go
@@ -0,0 +1,21 @@
+package control_test
+
+import (
+	"testing"
+	"time"
+)
+
+func TestShuffleA(t *testing.T) {
+	t.Log("Случайный тест А...")
+	time.Sleep(1 * time.Second)
+}
+
+func TestShuffleB(t *testing.T) {
+	t.Log("Случайный тест B...")
+	time.Sleep(1 * time.Second)
+}
+
+func TestShuffleC(t *testing.T) {
+	t.Log("Случайный тест C...")
+	time.Sleep(1 * time.Second)
+}
