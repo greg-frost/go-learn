a7dba720df0b1d56cd3a07a9bbf0c49e77f15c3e
Григорий Морозов <greg-frost@yandex.ru>
2026-05-25 08:09:06 +0300

    Learn httptest

---
+++ httptest/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+// Текст и HTML-разметка
+const (
+	text = "Go, Baby, Go!"
+	html = `
+		<doctype html>
+		<html>
+			<head>
+				<title>Go Server</title>
+			</head>
+			<body>
+				<h1>Go Server</h1>
+				<p>Go, Baby, Go!</p>
+			</body>
+		</html>
+	`
+)
+
+// TXT-обработчик
+func textHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, text)
+}
+
+// HTML-обработчик
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, html)
+}
+
+func main() {
+	fmt.Println(" \n[ ТЕСТИРОВАНИЕ HTTP ]\n ")
+
+	// Обработчики
+	http.HandleFunc("/", textHandler)
+	http.HandleFunc("/html/", htmlHandler)
+
+	// Запуск сервера
+	fmt.Println("Ожидаю соединений...")
+	fmt.Println("(на http://localhost:8080)")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
