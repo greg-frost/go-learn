0339a8d8a3216002479bc774c8d24edefcb5ebeb
Григорий Морозов <greg-frost@yandex.ru>
2026-05-28 11:38:05 +0300

    Fix httptest

--- httptest/main.go
+++ httptest/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 // Текст и HTML-разметка
@@ -37,6 +38,25 @@ func htmlHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, html)
 }
 
+// Клиент
+func Client(addr string) (int, string) {
+	time.Sleep(50 * time.Millisecond)
+
+	get, err := http.Get(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer get.Body.Close()
+	fmt.Printf("GET: %s --> %s\n", addr, get.Status)
+
+	body, err := io.ReadAll(get.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return get.StatusCode, string(body)
+}
+
 func main() {
 	fmt.Println(" \n[ ТЕСТИРОВАНИЕ HTTP ]\n ")
 
@@ -44,8 +64,13 @@ func main() {
 	http.HandleFunc("/", textHandler)
 	http.HandleFunc("/html/", htmlHandler)
 
+	// Запуск клиентов
+	go Client("http://localhost:8080")
+	go Client("http://localhost:8080/html")
+
 	// Запуск сервера
 	fmt.Println("Ожидаю соединений...")
 	fmt.Println("(на http://localhost:8080)")
+	fmt.Println()
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
