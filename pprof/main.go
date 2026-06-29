ecd41dc29e14ece929ba5a8939b29322328a8882
Григорий Морозов <greg-frost@yandex.ru>
2026-06-30 01:01:31 +0300

    Fix pprof

--- pprof/main.go
+++ pprof/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Простые числа
-func primes1(n int) bool {
+func Primes1(n int) bool {
 	k := math.Floor(float64(n/2 + 1))
 	for i := 2; i < int(k); i++ {
 		if (n % i) == 0 {
@@ -23,7 +23,7 @@ func primes1(n int) bool {
 }
 
 // Простые числа (оптимизированный вариант)
-func primes2(n int) bool {
+func Primes2(n int) bool {
 	for i := 2; i < n; i++ {
 		if (n % i) == 0 {
 			return false
@@ -33,16 +33,16 @@ func primes2(n int) bool {
 }
 
 // Числа Фибоначчи
-func fibo1(n int) int64 {
+func Fib1(n int) int64 {
 	if n == 0 || n == 1 {
 		return int64(n)
 	}
 	time.Sleep(time.Millisecond)
-	return int64(fibo2(n-1)) + int64(fibo2(n-2))
+	return int64(Fib2(n-1)) + int64(Fib2(n-2))
 }
 
 // Числа Фибоначчи (оптимизированный вариант)
-func fibo2(n int) int {
+func Fib2(n int) int {
 	fn := make(map[int]int)
 	for i := 0; i <= n; i++ {
 		var f int
@@ -79,7 +79,7 @@ func main() {
 	// Простые числа
 	var total int
 	for i := 2; i < 100000; i++ {
-		isPrime := primes1(i)
+		isPrime := Primes1(i)
 		if isPrime {
 			total++
 		}
@@ -89,7 +89,7 @@ func main() {
 	// Простые числа (оптимизированный вариант)
 	total = 0
 	for i := 2; i < 100000; i++ {
-		isPrime := primes2(i)
+		isPrime := Primes2(i)
 		if isPrime {
 			total++
 		}
@@ -100,7 +100,7 @@ func main() {
 	// Числа Фибоначии
 	fmt.Println("Числа Фибоначчи (1):")
 	for i := 1; i < 90; i++ {
-		fibo := fibo1(i)
+		fibo := Fib1(i)
 		fmt.Print(fibo, " ")
 	}
 	fmt.Println()
@@ -109,7 +109,7 @@ func main() {
 	// Числа Фибоначии (оптимизированный вариант)
 	fmt.Println("Числа Фибоначчи (2):")
 	for i := 1; i < 90; i++ {
-		fibo := fibo2(i)
+		fibo := Fib2(i)
 		fmt.Print(fibo, " ")
 	}
 	fmt.Println()
