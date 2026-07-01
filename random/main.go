431bcbdd70fd54e81c2f1260333673660c63de7e
Григорий Морозов <greg-frost@yandex.ru>
2026-07-02 00:12:11 +0300

    Fix random

--- random/main.go
+++ random/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Набор случайных чисел
-func randomInts(n, from, to int) []int {
+func RandomInts(n, from, to int) []int {
 	res := make([]int, n)
 	for i := 0; i < n; i++ {
 		res[i] = from + rand.Intn(to-from+1)
@@ -16,7 +16,7 @@ func randomInts(n, from, to int) []int {
 }
 
 // Случайная строка
-func randomString(n int, from, to byte) string {
+func RandomString(n int, from, to byte) string {
 	res := make([]byte, n)
 	for i := 0; i < n; i++ {
 		res[i] = from + byte(rand.Intn(int(to-from+1)))
@@ -29,30 +29,30 @@ func main() {
 
 	// Seed не задан
 	fmt.Println("Случайные числа:")
-	fmt.Println(randomInts(10, 1, 10))
-	fmt.Println(randomInts(10, 1, 10))
-	fmt.Println(randomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
 	fmt.Println()
 
 	// Seed задан определенно
 	fmt.Println("Неслучайные числа:")
 	rand.Seed(0)
-	fmt.Println(randomInts(10, 1, 10))
-	fmt.Println(randomInts(10, 1, 10))
-	fmt.Println(randomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
 	fmt.Println()
 
 	// Seed задан произвольно
 	fmt.Println("Снова случайные числа:")
 	rand.Seed(time.Now().Unix())
-	fmt.Println(randomInts(10, 1, 10))
-	fmt.Println(randomInts(10, 1, 10))
-	fmt.Println(randomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
+	fmt.Println(RandomInts(10, 1, 10))
 	fmt.Println()
 
 	// Случайные строки
 	fmt.Println("Случайные строки:")
-	fmt.Println(randomString(20, '!', '~'))
-	fmt.Println(randomString(20, 'A', 'Z'))
-	fmt.Println(randomString(20, 'a', 'z'))
+	fmt.Println(RandomString(20, '!', '~'))
+	fmt.Println(RandomString(20, 'A', 'Z'))
+	fmt.Println(RandomString(20, 'a', 'z'))
 }
